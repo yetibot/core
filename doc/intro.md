e66c49c4178f6305b3471d656f4b036c531a164e
Trevor Hartman <trevorhartman@gmail.com>
2013-12-16 20:34:28 -0700

    Init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to yetibot-core
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/yetibot_core/core_test.clj
@@ -0,0 +1,7 @@
+(ns yetibot-core.core-test
+  (:require [clojure.test :refer :all]
+            [yetibot-core.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
