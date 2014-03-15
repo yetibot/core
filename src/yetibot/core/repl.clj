73b170be98a0d2f54f6995a2bac1e6291ca3854e
Trevor Hartman <trevorhartman@gmail.com>
2014-03-14 23:58:38 -0600

    Add repl start function to start chat adapters

--- src/yetibot/core/repl.clj
+++ src/yetibot/core/repl.clj
@@ -1,18 +1,22 @@
 (ns yetibot.core.repl
   "Load this namespace when working with YetiBot in the REPL or during dev."
   (:require
-    [yetibot.core.db :refer [repl-start]]
+    [yetibot.core.db :as db]
     [yetibot.core.models.users :as users]
-    [yetibot.core.loader :refer [load-commands-and-observers load-ns]]))
+    [yetibot.core.loader :refer [load-commands-and-observers load-ns]]
+    [yetibot.core.adapters.campfire :as cf]
+    [yetibot.core.adapters.irc :as irc]))
 
 ; use a few non-network commands for testing
 (defn load-minimal []
   (require 'yetibot.core.commands.echo :reload)
   (require 'yetibot.core.commands.collections :reload))
 
-(load-minimal)
-
-(repl-start)
+(defn start []
+  (load-minimal)
+  (db/repl-start)
+  (cf/start)
+  (irc/start))
 
 (defn load-all []
   (future
