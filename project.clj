e1f00c0142ba51caaf1815893f5fdb36ccd5ef5f
Trevor Hartman <trevorhartman@gmail.com>
2014-04-11 12:35:54 -0600

    lein-release plugin: bumped version from 0.1.17 to 0.1.18-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot.core "0.1.17"
+(defproject yetibot.core "0.1.18-SNAPSHOT"
   :description "Core yetibot utilities, extracted for shared use among yetibot
                 and its various plugins"
   :url "https://github.com/devth/yetibot.core"
