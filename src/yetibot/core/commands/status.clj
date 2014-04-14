52af4ad83949efdf0a8b7bef66df088b084c65f7
Trevor Hartman <trevorhartman@gmail.com>
2014-04-13 20:29:18 -0600

    Get fresh db after setting status
    fix devth/yetibot#396

--- src/yetibot/core/commands/status.clj
+++ src/yetibot/core/commands/status.clj
@@ -1,5 +1,6 @@
 (ns yetibot.core.commands.status
   (:require
+    [yetibot.core.util :refer [with-fresh-db]]
     [taoensso.timbre :refer [info warn error]]
     [yetibot.core.models.status :as model]
     [clj-time [core :refer [ago minutes hours days weeks years months]]]
@@ -11,8 +12,7 @@
 (defn show-status
   "status # show statuses in the last 8 hours"
   [{:keys [chat-source]}]
-  (let [str-chat-source (pr-str chat-source)]
-    (model/format-sts (model/status-since chat-source (-> 8 hours ago)))))
+  (model/format-sts (model/status-since (pr-str chat-source) (-> 8 hours ago))))
 
 (defn show-status-since
   "status since <n> <minutes|hours|days|weeks|months> ago # show status since a given time"
@@ -30,7 +30,7 @@
   (let [str-chat-source (pr-str chat-source)]
     (info "add status in" str-chat-source ":" match)
     (model/add-status user str-chat-source match)
-    (show-status args)))
+    (with-fresh-db (show-status args))))
 
 (cmd-hook #"status"
           #"since (.+) (minutes*|hours*|days*|weeks*|months*)( ago)*" show-status-since
