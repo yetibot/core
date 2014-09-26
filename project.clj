ce118fbc68715b48c8eea30f34bd5041c503a760
Trevor Hartman <trevorhartman@gmail.com>
2014-09-26 17:32:13 -0600

    lein-release plugin: preparing 0.2.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot.core "0.2.1-SNAPSHOT"
+(defproject yetibot.core "0.2.1"
   :description "Core yetibot utilities, extracted for shared use among yetibot
                 and its various plugins"
   :url "https://github.com/devth/yetibot.core"
