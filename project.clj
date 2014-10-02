47a4356d7a875824eafe693700300dc93afa9617
Trevor Hartman <trevorhartman@gmail.com>
2014-10-01 20:20:34 -0600

    Bump to 0.2.2-S

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot.core "0.2.1"
+(defproject yetibot.core "0.2.2-SNAPSHOT"
   :description "Core yetibot utilities, extracted for shared use among yetibot
                 and its various plugins"
   :url "https://github.com/devth/yetibot.core"
