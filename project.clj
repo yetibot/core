2d20d79fafefca48cdb62d72b8d5514c9823048c
Trevor Hartman <trevorhartman@gmail.com>
2014-10-14 08:25:52 -0600

    Update datomic free to 0.9.4956

--- project.clj
+++ project.clj
@@ -59,7 +59,7 @@
                  [irclj "0.5.0-alpha4"]
 
                  ; database
-                 [com.datomic/datomic-free "0.8.3814"]
+                 [com.datomic/datomic-free "0.9.4956"]
                  [datomico "0.2.0"]
 
                  ; javascript evaluation
