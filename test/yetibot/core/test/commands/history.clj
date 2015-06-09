3d5bd151057b5b495068b2e7a217a0d63c449bf8
Robb Wright <audaxion@gmail.com>
2015-06-09 14:41:24 -0700

    Fix for broken history tests.

--- test/yetibot/core/test/commands/history.clj
+++ test/yetibot/core/test/commands/history.clj
@@ -1,9 +1,11 @@
 (ns yetibot.core.test.commands.history
   (:require
     [yetibot.core.db :as db]
+    [yetibot.core.config :refer [reload-config]]
     [yetibot.core.models.history :as h]
     [yetibot.core.commands.history :refer :all]))
 
+(reload-config "test/resources/test-config.edn")
 (db/repl-start)
 
 (history-cmd {:chat-source {:adapter :test :room "foo"}
