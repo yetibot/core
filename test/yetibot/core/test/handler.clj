1733db295cfaa8b667122f3dd188b7de1cd23e2f
Trevor Hartman <trevorhartman@gmail.com>
2015-11-11 00:57:02 -0500

    Add test case that freezes the parser

--- test/yetibot/core/test/handler.clj
+++ test/yetibot/core/test/handler.clj
@@ -33,4 +33,11 @@
 (deftest test-newline-cmd
   (let [multi-str (s/join \newline [1 2 3])]
     (is (= multi-str
-        (handle-unparsed-expr (str "echo " multi-str)))))
+        (handle-unparsed-expr (str "echo " multi-str))))))
+
+;; this freezes!
+#_(handle-unparsed-expr "echo Action ansible.command completed.
+                       {\"failed\": false, \"stderr\": \"\", \"return_code\": 0, \"succeeded\": true, \"stdout\": \"si-cluster-zk3 . success >> {\n    \"changed\": false,\n    \"ping\": \"pong\"\n}\n\nsi-cluster-zk2 . success >> {\n    \"changed\": false,\n    \"ping\": \"pong\"\n}\n\nsi-cluster-zk1 . success >> {\n    \"changed\": false,\n    \"ping\": \"pong\"\n}\n\"}\""
+                      )
+
+
