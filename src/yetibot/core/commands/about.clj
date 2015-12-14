b788deda9bdf107b598ec006bd58cd4398a1700b
Trevor Hartman <trevorhartman@gmail.com>
2015-12-14 14:25:25 -0700

    Add about command

---
+++ src/yetibot/core/commands/about.clj
@@ -0,0 +1,11 @@
+(ns yetibot.core.commands.about
+  (:require [yetibot.core.hooks :refer [cmd-hook]]))
+
+(defn about-cmd [& _]
+  ["Yetibot is a chat bot written in Clojure;"
+   "it wants to make your life easier;"
+   "it wants you to have fun."
+   "http://yetibot.com"])
+
+(cmd-hook #"about"
+  _ about-cmd)
