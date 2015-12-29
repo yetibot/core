fa88fbd903fec38a621e55982a2462c73917e91b
Trevor Hartman <trevorhartman@gmail.com>
2015-12-29 15:36:24 -0700

    Fixup the api based on simplified adapter uuid

--- src/yetibot/core/webapp/routes/api.clj
+++ src/yetibot/core/webapp/routes/api.clj
@@ -1,63 +1,33 @@
 (ns yetibot.core.webapp.routes.api
   (:require
-    [yetibot.core.webapp.views.common :as views]
     [yetibot.core.interpreter :refer [*chat-source*]]
     [compojure.route :as route]
     [compojure.handler :as handler]
     [compojure.response :as response]
     [yetibot.core.handler :refer [handle-unparsed-expr]]
-    [yetibot.core.chat :refer [chat-data-structure]]
+    [yetibot.core.chat :refer [*target* *adapter-uuid* chat-data-structure]]
     [taoensso.timbre :refer [info warn]]
-    ; [yetibot.core.adapters.campfire :refer [self]]
     [clojure.edn :as edn]
     [compojure.core :refer :all]))
 
-
-;; todo - bind *adapter* from hash instaed of all this messaging-fns crazyness
-(comment
-
-  ;; TODO: move *target* to chat. and maybe chat-source too?
-  ;; ok awesome, i can get rid of this nasty binding junk. right?
-
-  (defn determine-messaging-fns-and-target
-    "target should be a symbol, not a resolved var, so it can be dynamically bound"
-    [chat-source]
-    (let [adapter-ns (condp = (:adapter chat-source)
-                       :slack 'yetibot.core.adapters.slack
-                       :irc 'yetibot.core.adapters.irc
-                       :campfire 'yetibot.core.adapters.campfire)]
-      [(ns-resolve adapter-ns 'messaging-fns)
-       ; e.g. 'yetibot.core.adapters.slack/*target*
-       (symbol (str adapter-ns) "*target*")]))
-
-  (defn api [{:keys [chat-source text command token] :as params} req]
-    (info "/api called with params:" params)
-    (info "/api request:" req)
-    (when-let [body (:body req)] (info "/api body:" (slurp body)))
-    (cond
-      (empty? chat-source) "chat-source parameter is required!"
-      (and (empty? command) (empty? text)) "command or text parameter is required!"
-      :else (if-let [chat-source (edn/read-string chat-source)]
-              (do
-                (info "chat-source" chat-source)
-                (let [user {:username "api"}
-                      room (:room chat-source)
-                      res (or text (handle-unparsed-expr chat-source user command))
-                      [messaging-fns *target*] (determine-messaging-fns-and-target chat-source)]
-                  (binding [*messaging-fns* @messaging-fns
-                            *chat-source* chat-source]
-                    (info "*messaging-fns*:" *messaging-fns*)
-                    ; this is pretty gross. todo: refactor without runtime binding
-                    ; could avoid by having chat-data-structure bind *target*
-                    ; using (:room chat-source) if not bound.
-                    (eval `(binding [~*target* ~room]
-                             (~chat-data-structure ~res)
-                             ~res)))))
-              (str "invalid chat-source:" chat-source))))
-
-
-  )
+(defn api [{:keys [chat-source text command token] :as params} req]
+  (info "/api called with params:" params)
+  (info "/api request:" req)
+  (cond
+    (empty? chat-source) "chat-source parameter is required!"
+    (and (empty? command) (empty? text)) "command or text parameter is required!"
+    :else (if-let [chat-source (edn/read-string chat-source)]
+            (binding [*chat-source* chat-source
+                      *adapter-uuid* (:uuid chat-source)
+                      *target* (:room chat-source)]
+              (info "chat-source" chat-source)
+              (let [user {:username "api"}
+                    room (:room chat-source)
+                    res (or text (handle-unparsed-expr chat-source user command))]
+                (chat-data-structure res)
+                res))
+            (str "invalid chat-source:" chat-source))))
 
 (defroutes api-routes
-  #_(GET "/api" [& params :as req] (api params req))
-  #_(POST "/api" [& params :as req] (api params req)))
+  (GET "/api" [& params :as req] (api params req))
+  (POST "/api" [& params :as req] (api params req)))
