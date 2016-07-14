b89b636c7c73be94b9a6a76011e5b4cc7981049d
Trevor Hartman <trevorhartman@gmail.com>
2016-07-14 11:34:41 -0600

    Version 1.0.0-RC4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot.core "1.0.0-RC3"
+(defproject yetibot.core "1.0.0-RC4-SNAPSHOT"
   :description "Core yetibot utilities, extracted for shared use among yetibot
                 and its various plugins"
   :url "https://github.com/devth/yetibot.core"
