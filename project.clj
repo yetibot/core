3026d03358768f79472dddb6e646117c82178d21
Trevor Hartman <trevorhartman@gmail.com>
2016-12-23 12:33:12 -0700

    Remove alpha from version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot.core "0.4.0-alpha17-SNAPSHOT"
+(defproject yetibot.core "0.4.0-SNAPSHOT"
   :description "Core yetibot utilities, extracted for shared use among yetibot
                 and its various plugins"
   :url "https://github.com/devth/yetibot.core"
