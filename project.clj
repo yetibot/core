9fc4b4f4d54d3e09ddfb296f06585dcf9df10c84
Trevor Hartman <trevorhartman@gmail.com>
2017-12-05 09:33:59 -0700

    Version 0.4.17-alpha2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot.core "0.4.17-alpha2-SNAPSHOT"
+(defproject yetibot.core "0.4.17-alpha2"
   :description "Core yetibot utilities, extracted for shared use among yetibot
                 and its various plugins"
   :url "https://github.com/yetibot/yetibot.core"
