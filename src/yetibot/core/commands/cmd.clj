3118437bd3c1e1cdb9b3a7f8991144141e6f3dbd
Trevor Hartman <trevorhartman@gmail.com>
2017-12-15 17:11:09 -0700

    Add cmd command
    Fix yetibot/yetibot#703

---
+++ src/yetibot/core/commands/cmd.clj
@@ -0,0 +1,23 @@
+(ns yetibot.core.commands.cmd
+  (:require
+    [yetibot.core.hooks :refer [cmd-hook]]
+    [yetibot.core.handler :refer [extract-command config-prefix handle-unparsed-expr]]))
+
+(def command-pattern (re-pattern (str "^" config-prefix)))
+
+(defn strip-prefix
+  "Strips command prefix from the beginning of a string, if present"
+  [s]
+  (if (re-find command-pattern s)
+    (last (extract-command s))
+    s))
+
+(defn cmd
+  "cmd # evaluate a literal command. Strips configured cmd prefix (default !) from the beginning if present"
+  {:yb/cat #{:util}}
+  [{match :match}]
+  (handle-unparsed-expr
+    (strip-prefix match)))
+
+(cmd-hook #"cmd"
+  _ cmd)
