509e5391b7841aa8116260a0e74fcbd217735537
Trevor Hartman <trevorhartman@gmail.com>
2017-12-28 13:19:35 -0700

    Add graphql enpoint with eval query support

--- src/yetibot/core/webapp/handler.clj
+++ src/yetibot/core/webapp/handler.clj
@@ -1,15 +1,17 @@
 (ns yetibot.core.webapp.handler
-  (:require [compojure.core :refer [defroutes routes wrap-routes]]
-            [org.httpkit.server :refer [run-server]]
-            [yetibot.core.webapp.routes.home :refer [home-routes]]
-            [yetibot.core.webapp.routes.api :refer [api-routes]]
-            [yetibot.core.webapp.middleware :as middleware]
-            [yetibot.core.webapp.session :as session]
-            [yetibot.core.webapp.route-loader :as rl]
-            [compojure.route :as route]
-            [taoensso.timbre :as timbre]
-            [environ.core :refer [env]]
-            [clojure.tools.nrepl.server :as nrepl]))
+  (:require
+    [compojure.core :refer [defroutes routes wrap-routes]]
+    [org.httpkit.server :refer [run-server]]
+    [yetibot.core.webapp.routes.home :refer [home-routes]]
+    [yetibot.core.webapp.routes.api :refer [api-routes]]
+    [yetibot.core.webapp.routes.graphql :refer [graphql-routes]]
+    [yetibot.core.webapp.middleware :as middleware]
+    [yetibot.core.webapp.session :as session]
+    [yetibot.core.webapp.route-loader :as rl]
+    [compojure.route :as route]
+    [taoensso.timbre :as timbre]
+    [environ.core :refer [env]]
+    [clojure.tools.nrepl.server :as nrepl]))
 
 (defonce nrepl-server (atom nil))
 
@@ -34,9 +36,9 @@
 ;     (nrepl/stop-server server)))
 
 
+
 (defn init
-  "init will be called once when app is deployed as a servlet on an app server
-   such as Tomcat put any initialization code here"
+  "init will be called once on startup"
   []
   ; (start-nrepl)
   ;; start the expired session cleanup job
@@ -60,6 +62,7 @@
     (-> (apply routes
                home-routes
                api-routes
+               graphql-routes
                last-routes)
         middleware/wrap-base)))
 
@@ -76,3 +79,7 @@
   (when-not (nil? @web-server)
     (@web-server :timeout 100)
     (reset! web-server nil)))
+
+(defn restart-web-server []
+  (stop-web-server)
+  (start-web-server))
