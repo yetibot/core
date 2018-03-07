68d2177dbe60f7fc9f85f8d84eda98ce5cde8932
Trevor Hartman <trevorhartman@gmail.com>
2018-03-07 16:17:53 -0700

    Fix history model for pulling from history using chat source uuid

--- src/yetibot/core/models/history.clj
+++ src/yetibot/core/models/history.clj
@@ -10,66 +10,66 @@
 ;;;; read
 
 (defn history-for-chat-source
-  [{:keys [adapter room] :as chat-source}]
+  [{:keys [uuid room] :as chat-source}]
   "Retrieve a map of user to chat body"
   (query {:where/map
-          {:chat-source-adapter (pr-str adapter)
+          {:chat-source-adapter (pr-str uuid)
            :chat-source-room room}}))
 
 (defn count-entities
-  [{:keys [adapter room] :as chat-source}]
+  [{:keys [uuid room] :as chat-source}]
   (-> (query
         {:where/map
-         {:chat-source-adapter (pr-str adapter)
+         {:chat-source-adapter (pr-str uuid)
           :chat-source-room room}
          :select/clause "COUNT(*) as count"})
       first
       :count))
 
 (defn head
-  [{:keys [adapter room] :as chat-source} n]
+  [{:keys [uuid room] :as chat-source} n]
   (query
     {:where/map
-     {:chat-source-adapter (pr-str adapter)
+     {:chat-source-adapter (pr-str uuid)
       :chat-source-room room}
      :limit/clause (str n)}))
 
 (defn tail
-  [{:keys [adapter room] :as chat-source} n]
+  [{:keys [uuid room] :as chat-source} n]
   (-> (query
         {:where/map
-         {:chat-source-adapter (pr-str adapter)
+         {:chat-source-adapter (pr-str uuid)
           :chat-source-room room}
          :order/clause "created_at DESC"
          :limit/clause (str n)})
       reverse))
 
 (defn random
-  [{:keys [adapter room] :as chat-source}]
+  [{:keys [uuid room] :as chat-source}]
   (query
     {:where/map
-     {:chat-source-adapter (pr-str adapter)
+     {:chat-source-adapter (pr-str uuid)
       :chat-source-room room}
      :order/clause "random()" ;; possibly slow on large tables
      :limit/clause "1"}))
 
-(defn grep [{:keys [adapter room] :as chat-source} pattern]
+(defn grep [{:keys [uuid room] :as chat-source} pattern]
   (query
     {:where/clause (str "chat_source_adapter=? AND chat_source_room=?"
                         " AND body ~ ?")
-     :where/args  [(pr-str adapter) room pattern]}))
+     :where/args  [(pr-str uuid) room pattern]}))
 
 (defn last-chat-for-room
   "Takes chat source and returns the last chat for the room.
    `cmd?` is a boolean specifying whether it should be the last yetibot command
    or a normal chat. Useful for `that` commands."
   ([chat-source cmd?] (last-chat-for-room chat-source cmd? 1))
-  ([{:keys [adapter room]} cmd? history-count]
+  ([{:keys [uuid room]} cmd? history-count]
    (query {:limit/clause history-count
            :order/clause "created_at DESC"
            :where/map
            {:is-command cmd?
-            :chat-source-adapter (pr-str adapter)
+            :chat-source-adapter (pr-str uuid)
             :chat-source-room room}})))
 
 ;; Note: these aren't currently used. If the eventually are, we should figure
@@ -77,31 +77,31 @@
 ;; cmd or non-cmd items for a given chat-source.
 (defn non-cmd-items
   "Return `chat-item` only if it doesn't match any regexes in `history-ignore`"
-  [{:keys [adapter room] :as chat-source}]
+  [{:keys [uuid room] :as chat-source}]
   (query {:where/map
           {:is-command false
-           :chat-source-adapter (pr-str adapter)
+           :chat-source-adapter (pr-str uuid)
            :chat-source-room room}}))
 
 (defn cmd-only-items
   "Return `chat-item` only if it does match any regexes in `history-ignore`"
-  [{:keys [adapter room] :as chat-source}]
+  [{:keys [uuid room] :as chat-source}]
   (query {:where/map
             {:is-command true
-             :chat-source-adapter (pr-str adapter)
+             :chat-source-adapter (pr-str uuid)
              :chat-source-room room}}))
 
 (defn items-for-user
   "Ordered by most recent. Used by the `!` command.
    Filters out all `!` commands to prevent infinite recursion."
   [{:keys [chat-source user cmd? limit]}]
-  (let [{:keys [adapter room]} chat-source
+  (let [{:keys [uuid room]} chat-source
         limit (or limit 1)]
     (reverse
       (query
           {:where/clause (str "chat_source_adapter=? AND chat_source_room=?"
                               " AND is_command=? AND body NOT LIKE '!!%'")
-           :where/args  [(pr-str adapter) room cmd?]
+           :where/args  [(pr-str uuid) room cmd?]
            :limit/clause limit
            :order/clause "created_at DESC"}))))
 
