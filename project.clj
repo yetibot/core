9eba7f07088b95b57c1cd263ef2dc02b2db5f433
Trevor Hartman <trevorhartman@gmail.com>
2019-02-18 16:14:27 -0700

    Version 0.4.68-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot.core "0.4.67"
+(defproject yetibot.core "0.4.68-SNAPSHOT"
   :description "Core yetibot utilities, extracted for shared use among yetibot
                 and its various plugins"
   :url "https://github.com/yetibot/yetibot.core"
