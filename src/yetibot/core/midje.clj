dd6f02d4511a60c996a2f7445fc7424d228b8226
Jereme Corrado <jereme@zoion.net>
2019-04-03 13:02:02 -0400

    Moved our Midje custom checkers to yetibot.core.midje ns (#82)

---
+++ src/yetibot/core/midje.clj
@@ -0,0 +1,18 @@
+(ns yetibot.core.midje
+  (:require
+   [midje.sweet :refer [defchecker checker]]
+   [midje.checking.core :refer [extended-=]]))
+
+;; A few Midje checkers providing Extended Equality tests of standard
+;; keys in the map returned by commands supporting piped data.  These
+;; are intended to be used in *.command.* tests.
+
+(defn mk-checker
+  [expected key]
+  (checker
+   [actual]
+   (extended-= (key actual) expected)))
+
+(defchecker value [expected] (mk-checker expected :result/value))
+(defchecker data  [expected] (mk-checker expected :result/data))
+(defchecker error [expected] (mk-checker expected :result/error))
