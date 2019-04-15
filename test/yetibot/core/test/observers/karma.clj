6de30c098e5ec53fad29cb50a5ab042717b9d84c
Jereme Corrado <jereme@zoion.net>
2019-04-14 22:32:20 -0400

    Added Karma observer tests

--- test/yetibot/core/test/observers/karma.clj
+++ test/yetibot/core/test/observers/karma.clj
@@ -3,7 +3,7 @@
    [midje.sweet :refer [namespace-state-changes with-state-changes fact => truthy]]
    [yetibot.core.observers.karma :refer [pos-reaction neg-reaction reaction-hook message-hook]]
    [yetibot.core.models.karma :as model ]
-   [yetibot.core.commands.karma :refer [pos-emoji neg-emoji]]
+   [yetibot.core.commands.karma :refer [pos-emoji neg-emoji error]]
    [yetibot.core.db :as db]
    [clj-time.core :as time]
    [clj-time.coerce :as time.coerce]))
@@ -12,16 +12,25 @@
 (def test-user (str "test-user-" epoch))
 (def test-voter (str "test-voter-" epoch))
 (def test-note (str "test-note-" epoch))
-;;(def test-score 1000000)
-;;(def _) "entire-match not used by test"
 
-;; The reaction event passed to our observers
+;; Mock reaction event passed to our observers
 (def reaction-event {:user {:id test-voter :name test-voter}
                      :message-user {:id test-user}})
 
 (def pos-reaction-event (assoc reaction-event :reaction pos-reaction))
-
 (def neg-reaction-event (assoc reaction-event :reaction neg-reaction))
+
+;; Mock message event passed to our observers
+(def message-event {:user {:id test-voter :name test-voter}})
+
+(defn mk-message-event
+  [emoji, user]
+  (assoc message-event :body (format "%s @%s" emoji user)))
+
+(def pos-message-event (mk-message-event pos-emoji test-user))
+(def neg-message-event (mk-message-event neg-emoji test-user))
+(def pos-message-self-event (mk-message-event pos-emoji test-voter))
+(def neg-message-self-event (mk-message-event neg-emoji test-voter))
 
 (defn- reply-is?
   [emoji s]
@@ -29,7 +38,6 @@
     (string? (re-find re s))))
 
 (def inc-success? (partial reply-is? pos-emoji))
-
 (def dec-success? (partial reply-is? neg-emoji))
 
 
@@ -38,31 +46,29 @@
 (with-state-changes [(after :facts (model/delete-user! test-user))]
 
   ;; Reaction Observer
-  
   (fact "reaction-hook can increment karma for another user"
-    (reaction-hook pos-reaction-event) => inc-success?)
+        (reaction-hook pos-reaction-event) => inc-success?)
 
   (fact "reaction-hook can decrement karma for another user"
-    (reaction-hook neg-reaction-event) => dec-success?)
+        (reaction-hook neg-reaction-event) => dec-success?)
 
   (fact "reaction-hook allows a user to decrement their own karma"
-    (let [e (assoc-in neg-reaction-event [:message-user :id] test-voter )]
-      (reaction-hook e)) => dec-success?)
+        (let [e (assoc-in neg-reaction-event [:message-user :id] test-voter)]
+          (reaction-hook e) => dec-success?))
 
-  (fact "reaction-hook precludes a users from incrementing their own karma")
+  (fact "reaction-hook precludes a users from incrementing their own karma"
+        (let [e (assoc-in pos-reaction-event [:message-user :id] test-voter)]
+          (reaction-hook e) => (-> error :karma :result/error)))
 
   ;; Message Observer
+  (fact "message-hook can increment karma for another user"
+        (message-hook pos-message-event) => inc-success?)
 
-  (fact "message-hook can increment karma for another user")
+  (fact "message-hook can decrement karma for another user"
+        (message-hook neg-message-event) => dec-success?)
 
-  (fact "message-hook can increment karma for another user and include a note")
+  (fact "message-hook allows a user to decrement their own karma"
+        (message-hook neg-message-self-event) => dec-success?)
 
-  (fact "message-hook can decrement karma for another user")
-
-  (fact "message-hook can decrement karma for another user and include a note")
-
-  (fact "message-hook allows a user to decrement their own karma")
-
-  (fact "message-hook precludes a users from incrementing their own karma")
-
-  )
+  (fact "message-hook precludes a users from incrementing their own karma"
+        (message-hook pos-message-self-event) => (-> error :karma :result/error)))
