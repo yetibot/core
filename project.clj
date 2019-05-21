27e51f69afe6e4f73d2a873e24ed601985c590f1
Trevor Hartman <trevorhartman@gmail.com>
2019-05-21 15:50:23 -0600

    Switch project from yetibot.core to yetibot/core

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot.core "_"
+(defproject yetibot/core "_"
   :description "Core yetibot utilities, extracted for shared use among yetibot
                 and its various plugins"
   :url "https://github.com/yetibot/yetibot.core"
