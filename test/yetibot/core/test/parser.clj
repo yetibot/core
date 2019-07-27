da5748ca7c9a95a8f2faa1c60cac799e52682f88
Trevor Hartman <trevorhartman@gmail.com>
2019-07-27 17:36:34 -0600

    Migrate the parser tests to Midje

--- test/yetibot/core/test/parser.clj
+++ test/yetibot/core/test/parser.clj
@@ -1,136 +1,193 @@
 (ns yetibot.core.test.parser
   (:require
     [clojure.pprint :refer [pprint]]
+    [midje.sweet :refer [fact facts =>]]
+    [yetibot.core.models.karma :refer :all]
     [yetibot.core.parser :refer :all]
-    [instaparse.core :as insta]
-    [clojure.test :refer :all]))
+    [instaparse.core :as insta]))
 
-(deftest single-cmd-test
-  (is (= (parser "uptime")
-         [:expr [:cmd [:words "uptime"]]]))
-  (is (= (parser "echo qux")
-         [:expr [:cmd [:words "echo" [:space " "] "qux"]]])
-      "Single commands should be parsed"))
+(facts "single commands should be parsed"
+  (parser "uptime") => [:expr [:cmd [:words "uptime"]]]
+  (parser "echo qux") => [:expr [:cmd [:words "echo" [:space " "] "qux"]]])
 
-(deftest neighboring-sub-exprs
-  (is (= (parser "echo $(echo foo)bar")
-         [:expr [:cmd [:words "echo" [:space " "] [:sub-expr [:expr [:cmd [:words "echo" [:space " "] "foo"]]]] "bar"]]])))
+(fact "Neighboring sub expressions are parsed"
+  (parser "echo $(echo foo)bar") =>
+  [:expr [:cmd [:words
+                "echo" [:space " "]
+                [:sub-expr [:expr [:cmd [:words "echo" [:space " "] "foo"]]]]
+                "bar"]]])
 
-(deftest sub-expr-evaluation-test
-  (let [expr-tree (parser "echo foo `echo foo` bar")]
-    (is (= expr-tree
-           [:expr [:cmd [:words "echo" [:space " "] "foo" [:space " "]
-                         [:sub-expr
-                          [:expr [:cmd [:words "echo" [:space " "] "foo"]]]]
-                         [:space " "] "bar"]]]
-           ))
-    (let [{:keys [value]} (transformer expr-tree)]
-      (is (= "foo foo bar" value)))))
+(fact "Sub exprs should be parsed"
+  (parser "echo foo `echo foo` bar") =>
+  [:expr [:cmd [:words "echo" [:space " "] "foo" [:space " "]
+                [:sub-expr
+                 [:expr [:cmd [:words "echo" [:space " "] "foo"]]]]
+                [:space " "] "bar"]]])
 
-(deftest piped-cmd-test
-  (is
-    (= (parser "echo hello | echo bar")
-       [:expr [:cmd [:words "echo" [:space " "] "hello"]] [:cmd [:words "echo" [:space " "] "bar"]]])
-    "Piped commands should be parsed"))
+(fact "Piped commands should be parsed"
+  (parser "echo hello | echo bar") =>
+  [:expr
+   [:cmd [:words "echo" [:space " "] "hello"]]
+   [:cmd [:words "echo" [:space " "] "bar"]]])
 
-(deftest sub-expr-test
-  (is
-    (=
-     (parser "echo `catfact` | echo It is known:")
-     [:expr [:cmd [:words "echo" [:space " "] [:sub-expr [:expr [:cmd [:words "catfact"]]]]]] [:cmd [:words "echo" [:space " "] "It" [:space " "] "is" [:space " "] "known:"]]])
-    "Backtick sub-expressions should be parsed")
-  (is
-    (=
-     (parser "echo $(random)")
-     [:expr [:cmd [:words "echo" [:space " "] [:sub-expr [:expr [:cmd [:words "random"]]]]]]])
-    "Standard sub-expressions should be parsed"))
+(fact "Backtick sub-expressions should be parsed"
+  (parser "echo `catfact` | echo It is known:") =>
+  [:expr
+   [:cmd
+    [:words "echo" [:space " "] [:sub-expr [:expr [:cmd [:words "catfact"]]]]]]
+   [:cmd [:words "echo" [:space " "] "It" [:space " "] "is" [:space " "] "known:"]]])
 
-(deftest nested-sub-expr-test
-  (is
-    (=
-     (parser "random | buffer | echo `number $(buffer | peek)`")
-     [:expr [:cmd [:words "random"]] [:cmd [:words "buffer"]] [:cmd [:words "echo" [:space " "] [:sub-expr [:expr [:cmd [:words "number" [:space " "] [:sub-expr [:expr [:cmd [:words "buffer"]] [:cmd [:words "peek"]]]]]]]]]]])
-    "Nested sub-expressions should be parsed")
-  (is
-    (=
-     (parser
-       "echo foo
-        $(echo bar)")
-     [:expr [:cmd [:words "echo" [:space " "] "foo\n" [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:sub-expr [:expr [:cmd [:words "echo" [:space " "] "bar"]]]]]]])
-    "Expressions with newlines should preserve the newline")
-  (is
-    (=
-     (parser
-       "urban random | buffer | echo `meme wizard: what is $(buffer peek | head)?`
-        `meme chemistry: a $(buffer peek | head) is $(buffer peek | head 2 | tail)`")
-     [:expr [:cmd [:words "urban" [:space " "] "random"]] [:cmd [:words "buffer"]] [:cmd [:words "echo" [:space " "] [:sub-expr [:expr [:cmd [:words "meme" [:space " "] "wizard:" [:space " "] "what" [:space " "] "is" [:space " "] [:sub-expr [:expr [:cmd [:words "buffer" [:space " "] "peek"]] [:cmd [:words "head"]]]] "?"]]]] "\n" [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:sub-expr [:expr [:cmd [:words "meme" [:space " "] "chemistry:" [:space " "] "a" [:space " "] [:sub-expr [:expr [:cmd [:words "buffer" [:space " "] "peek"]] [:cmd [:words "head"]]]] [:space " "] "is" [:space " "] [:sub-expr [:expr [:cmd [:words "buffer" [:space " "] "peek"]] [:cmd [:words "head" [:space " "] "2"]] [:cmd [:words "tail"]]]]]]]]]]])
-    "Complex nested sub-expressions with newlines should be parsed"))
+(fact "Standard sub-expressions should be parsed"
+  (parser "echo $(random)") =>
+  [:expr [:cmd [:words "echo" [:space " "] [:sub-expr [:expr [:cmd [:words "random"]]]]]]])
 
-(deftest literal-test
-  (is
-    (= (parser "alias foo = \"bar\"")
-       [:expr [:cmd [:words "alias" [:space " "] "foo" [:space " "] "=" [:space " "] [:literal "\"" "bar" "\""]]]]))
-  (is
-    (= (parser "meme foo: \"lol")
-       [:expr [:cmd [:words "meme" [:space " "] "foo:" [:space " "] "\"" "lol"]]]))
-  (is
-    (= (parser "foo \"lol | foo\"")
-       [:expr [:cmd [:words "foo" [:space " "] [:literal "\"" "lol | foo" "\""]]]]))
-  (is
-    (= (parser "foo \"lol | foo")
-       [:expr [:cmd [:words "foo" [:space " "] "\"" "lol"]] [:cmd [:words "foo"]]])))
+(fact "Nested sub-expressions should be parsed"
+  (parser "random | buffer | echo `number $(buffer | peek)`") =>
+  [:expr
+   [:cmd [:words "random"]]
+   [:cmd [:words "buffer"]]
+   [:cmd
+    [:words
+     "echo"
+     [:space " "]
+     [:sub-expr
+      [:expr
+       [:cmd
+        [:words
+         "number"
+         [:space " "]
+         [:sub-expr
+          [:expr [:cmd [:words "buffer"]] [:cmd [:words "peek"]]]]]]]]]]])
 
-(deftest unmatched-parens-test
-  (is
-    (= (parser " foo)")
-       [:expr [:cmd [:words [:space " "] "foo" ")"]]]))
-  (is
-    (= (parser "foo)")
-       [:expr [:cmd [:words "foo" ")"]]]))
-  (is
-    (= (parser "foo (")
-       [:expr [:cmd [:words "foo" [:space " "] "("]]]))
-  (is
-    (= (parser " foo")
-       [:expr [:cmd [:words [:space " "] "foo"]]])))
+(fact "Expressions with newlines should preserve the newline"
+  (parser "echo foo $(echo bar)") =>
+  [:expr
+   [:cmd
+    [:words
+     "echo"
+     [:space " "]
+     "foo"
+     [:space " "]
+     [:sub-expr [:expr [:cmd [:words "echo" [:space " "] "bar"]]]]]]])
 
-(deftest special-character-test
-  (is (= (parser "foo \"$\"")
-         [:expr [:cmd [:words "foo" [:space " "] [:literal "\"" "$" "\""]]]])
-      "Parsing a special character works if it's a literal")
-  (is (= (parser "foo $")
-         [:expr [:cmd [:words "foo" [:space " "] "$"]]])
-      "Parsing with a sub-expr special character works as long as it doesn't fit
-       the shape of the beginning of a sub-expr, e.g. $(....."))
+(fact "Complex nested sub-expressions with newlines should be parsed"
+  (parser
+    "urban random | buffer | echo `meme wizard: what is $(buffer peek | head)?` `meme chemistry: a $(buffer peek | head) is $(buffer peek | head 2 | tail)`")
+  =>
+  [:expr
+   [:cmd [:words "urban" [:space " "] "random"]]
+   [:cmd [:words "buffer"]]
+   [:cmd
+    [:words
+     "echo"
+     [:space " "]
+     [:sub-expr
+      [:expr
+       [:cmd
+        [:words
+         "meme"
+         [:space " "]
+         "wizard:"
+         [:space " "]
+         "what"
+         [:space " "]
+         "is"
+         [:space " "]
+         [:sub-expr
+          [:expr
+           [:cmd [:words "buffer" [:space " "] "peek"]]
+           [:cmd [:words "head"]]]]
+         "?"]]]]
+     [:space " "]
+     [:sub-expr
+      [:expr
+       [:cmd
+        [:words
+         "meme"
+         [:space " "]
+         "chemistry:"
+         [:space " "]
+         "a"
+         [:space " "]
+         [:sub-expr
+          [:expr
+           [:cmd [:words "buffer" [:space " "] "peek"]]
+           [:cmd [:words "head"]]]]
+         [:space " "]
+         "is"
+         [:space " "]
+         [:sub-expr
+          [:expr
+           [:cmd [:words "buffer" [:space " "] "peek"]]
+           [:cmd [:words "head" [:space " "] "2"]]
+           [:cmd [:words "tail"]]]]]]]]]]])
 
-(deftest regex-pipe-test
-  (is (= (parser "grep foo|bar")
-         [:expr [:cmd [:words "grep" [:space " "] "foo" "|" "bar"]]])
-      "Parser should be able to handle a pipe without whitespace as a literal,
-       as in the case of regexes")
-  (is (= (parser "grep foo|bar|baz")
-         [:expr [:cmd [:words "grep" [:space " "] "foo" "|" "bar" "|" "baz"]]])
-      "Parser should be able to parse repeated pipes without surrounding whitespace"))
+(facts "literals should be parsed"
+  (parser "alias foo = \"bar\"") =>
+  [:expr [:cmd [:words "alias" [:space " "] "foo" [:space " "] "=" [:space " "] [:literal "\"" "bar" "\""]]]]
 
-(deftest significant-whitespace-test
-  (is (= (parser "list 1, 2, 3 | join        ")
-         [:expr [:cmd [:words "list" [:space " "] "1," [:space " "] "2," [:space " "] "3"]] [:cmd [:words "join" [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "]]]])
-      "Parser should preserve whitespace")
-  (is (= (parser "list 1, 2, 3 | join  | echo hi")
-         [:expr [:cmd [:words "list" [:space " "] "1," [:space " "] "2," [:space " "] "3"]] [:cmd [:words "join" [:space " "]]] [:cmd [:words "echo" [:space " "] "hi"]]])
-      "Parser should preserve whitespace"))
+  (parser "meme foo: \"lol") =>
+  [:expr [:cmd [:words "meme" [:space " "] "foo:" [:space " "] "\"" "lol"]]]
 
+  (parser "foo \"lol | foo\"") =>
+  [:expr [:cmd [:words "foo" [:space " "] [:literal "\"" "lol | foo" "\""]]]]
+
+  (parser "foo \"lol | foo") =>
+  [:expr [:cmd [:words "foo" [:space " "] "\"" "lol"]] [:cmd [:words "foo"]]])
+
+(facts "unmatched parens should be ignored by the parser"
+  (parser " foo)") =>
+  [:expr [:cmd [:words [:space " "] "foo" ")"]]]
+
+  (parser "foo)") =>
+  [:expr [:cmd [:words "foo" ")"]]]
+
+  (parser "foo (") =>
+  [:expr [:cmd [:words "foo" [:space " "] "("]]]
+
+  (parser " foo") =>
+  [:expr [:cmd [:words [:space " "] "foo"]]])
+
+(fact
+  "Parsing a special character works if it's a literal"
+  (parser "foo \"$\"") =>
+  [:expr [:cmd [:words "foo" [:space " "] [:literal "\"" "$" "\""]]]])
+
+(fact
+  "Parsing with a sub-expr special character works as long as it doesn't fit
+   the shape of the beginning of a sub-expr, e.g. $(....."
+  (parser "foo $") =>
+  [:expr [:cmd [:words "foo" [:space " "] "$"]]])
+
+(fact
+  "Parser should be able to handle a pipe without whitespace as a literal, as in
+   the case of regexes"
+  (parser "grep foo|bar") =>
+  [:expr [:cmd [:words "grep" [:space " "] "foo" "|" "bar"]]])
+
+(fact
+  "Parser should be able to parse repeated pipes without surrounding whitespace"
+  (parser "grep foo|bar|baz") =>
+  [:expr [:cmd [:words "grep" [:space " "] "foo" "|" "bar" "|" "baz"]]])
+
+(fact "Parser should preserve whitespace"
+  (parser "list 1, 2, 3 | join        ") =>
+  [:expr
+   [:cmd [:words "list" [:space " "] "1," [:space " "] "2," [:space " "] "3"]]
+   [:cmd [:words "join" [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "]]]])
+
+(fact "Parser should preserve whitespace"
+  (parser "list 1, 2, 3 | join  | echo hi") =>
+  [:expr [:cmd [:words "list" [:space " "] "1," [:space " "] "2," [:space " "] "3"]] [:cmd [:words "join" [:space " "]]] [:cmd [:words "echo" [:space " "] "hi"]]])
 
 ;; Failing test for https://github.com/devth/yetibot/issues/423
 ;; the problem is the parser tries to close the sub-expression too soon, leaving
 ;; a trailing ) outside of the sub-expr.
-#_(deftest preserve-matched-parens-in-subexpr
-  (is
-    (= (parser "hi $((hi))")
-       [:expr [:cmd [:words "hi" [:space " "] [:expr [:cmd [:words "(" "hi" ")"]]]]]]))
-  (is (=
-       (parser "echo $(clj (+ 1 1))")
-       [:expr [:cmd [:words "echo" [:space " "] [:expr [:cmd [:words "clj" [:space " "] "(" "+" [:space " "] "1" [:space " "] "1" ")"]]]]]])))
+#_(facts "preserve matched parens in subexpr"
+    (parser "hi $((hi))") =>
+    [:expr [:cmd [:words "hi" [:space " "] [:expr [:cmd [:words "(" "hi" ")"]]]]]]
+    (parser "echo $(clj (+ 1 1))") =>
+    [:expr [:cmd [:words "echo" [:space " "] [:expr [:cmd [:words "clj" [:space " "] "(" "+" [:space " "] "1" [:space " "] "1" ")"]]]]]])
 
 ;; unparsing
 
@@ -138,12 +195,10 @@
 
 (def sub-expr-unparse-sample "foo | echo $(echo `foo`)")
 
-(deftest unparse-test
-  (testing "Ability to unparse an expression back into its original string form"
-    (is (= pipes-sample (unparse (parser pipes-sample)))))
+(fact "it can unparse an expression back into its original string form"
+  (unparse (parser pipes-sample)) => pipes-sample)
 
-  (testing "Ability to unparse expression with backtick sub-expressions are
-            reconstructed using $() sub-expr syntax i.e. the backticks are not
-            preserved."
-    (is (= "foo | echo $(echo $(foo))"
-           (unparse (parser sub-expr-unparse-sample))))))
+(fact "it can unparse expression with backtick sub-expressions are reconstructed
+       using $() sub-expr syntax i.e. the backticks are not preserved."
+  (unparse (parser sub-expr-unparse-sample)) =>
+  "foo | echo $(echo $(foo))")
