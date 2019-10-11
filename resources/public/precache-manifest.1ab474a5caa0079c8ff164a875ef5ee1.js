8572b960a3dc2cb755198896554db0e90780cb2f
Trevor Hartman <thartman@ebay.com>
2019-10-11 11:53:41 -0600

    Include yetibot-dashboard in resources

---
+++ resources/public/precache-manifest.1ab474a5caa0079c8ff164a875ef5ee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c106e515c9b72f8b460",
+    "url": "/static/css/main.2aeb9be2.chunk.css"
+  },
+  {
+    "revision": "7c106e515c9b72f8b460",
+    "url": "/static/js/main.7c106e51.chunk.js"
+  },
+  {
+    "revision": "5537599578ce15920e77",
+    "url": "/static/js/1.55375995.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "144cb52a1f2ca83fdec2a3506fb93496",
+    "url": "/static/media/yetibot_lambda_blue_with_grey.144cb52a.svg"
+  },
+  {
+    "revision": "30b9e459780c671287cbd76d6db35158",
+    "url": "/index.html"
+  }
+];
