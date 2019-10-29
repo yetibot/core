d316d178313c1f18752030714f29dc9080c463ae
Trevor Hartman <trevorhartman@gmail.com>
2019-10-29 13:28:41 -0600

    Allow sub expressions in xargs (#114)
    * Port command_info test to Midje
    
    * Allow sub-exprs in command-execution-info

--- test/yetibot/core/test/util/command_info.clj
+++ test/yetibot/core/test/util/command_info.clj
@@ -1,37 +1,46 @@
 (ns yetibot.core.test.util.command-info
   (:require
-    yetibot.core.commands.collections
+    [midje.sweet :refer [fact => truthy falsey]]
     [yetibot.core.parser :refer [parser]]
-    yetibot.core.commands.echo
-    [clojure.test :refer :all]
-    [yetibot.core.util.command-info :refer :all]))
+    [yetibot.core.util.command-info :refer :all]
+    yetibot.core.commands.collections
+    yetibot.core.commands.echo))
 
-(deftest test-simple-command?
-  (is (simple-command? (parser "echo"))
-      "Should return true for a very simple command")
+(fact
+ "simple-command? should return true for a very simple command"
+ (simple-command? (parser "echo")) => truthy)
 
-  (is (not (simple-command? (parser "echo | echo")))
-      "Should be false on piped commands")
+(fact
+ "simple-command? should be false on piped commands"
+ (simple-command? (parser "echo | echo")) => falsey)
 
-  (is (not (simple-command? (parser "echo `bar`")))
-      "Should be false on commands with sub-expressions"))
+(fact
+ "simple-command? should be true on commands with sub-expressions"
+ (simple-command? (parser "echo `bar`")) => truthy)
 
-(deftest test-command-execution-info
-  (testing "Fake command"
-    (is
-      (=
-       {:parse-tree [:expr [:cmd [:words "foo" [:space " "] "bar"]]]
-        :sub-commands nil
-        :matched-sub-cmd nil
-        :match nil
-        :command "foo"
-        :command-args "bar"}
-       (command-execution-info "foo bar"))))
-  (testing "Real command with opts"
-    (let [{:keys [result match command-args]}
-          (command-execution-info
-            "head 2" {:run-command? true
-                      :opts ["one" "two" "three"]})]
-      (= match ["2" "2"])
-      (= result ["one" "two"])
-      (= command-args "2"))))
+(fact
+ "command-execution-info returns partial data on an unknown command"
+ (command-execution-info "foo bar")
+ => {:parse-tree [:expr [:cmd [:words "foo" [:space " "] "bar"]]]
+     :sub-commands nil
+     :matched-sub-cmd nil
+     :match nil
+     :command "foo"
+     :command-args "bar"})
+
+(fact
+ "command-execution-info runs top level command even when a sub-expr is included"
+ (command-execution-info
+  "echo foo bar `echo qux`"
+  {:run-command? true}) => truthy)
+
+(fact
+ "command-execution-info returns correct data for a known command with opts"
+ (let [{:keys [result match command-args]}
+       (command-execution-info
+        "head 2" {:run-command? true
+                  :opts ["one" "two" "three"]})]
+   match => ["2" "2"]
+   result => {:result/value ["one" "two"]}
+   command-args => "2"))
+
