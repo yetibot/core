16b77c8282e101e7a2dd0240b0f626c0613beecc
Trevor Hartman <trevorhartman@gmail.com>
2020-03-20 13:36:43 -0600

    Make karma channel-local by default

--- src/yetibot/core/models/karma.clj
+++ src/yetibot/core/models/karma.clj
@@ -4,40 +4,52 @@
    [clj-time.coerce :as time.coerce]))
 
 (defn add-score-delta!
-  [user-id voter-id points note]
-  (db/create {:user-id user-id
+  [{uuid :uuid room :room} user-id voter-id points note]
+  (db/create {:chat-source-adapter (pr-str uuid)
+              :chat-source-room room
+              :user-id user-id
               :voter-id voter-id
               :points points
               :note note}))
 
 (defn get-score
-  [user-id]
+  [{uuid :uuid room :room} user-id]
   (let [score (-> (db/query {:select/clause "SUM(points) as score"
-                             :where/map {:user-id user-id}})
+                             :where/map {:user-id user-id
+                                         :chat-source-adapter (pr-str uuid)
+                                         :chat-source-room room}})
                   first :score)]
     (if (nil? score) 0 score)))
 
 (defn get-notes
-  ([user-id] (get-notes user-id 3))
-  ([user-id cnt]
+  ([{uuid :uuid room :room} user-id]
+   (get-notes {uuid :uuid room :room} user-id 3))
+  ([{uuid :uuid room :room} user-id cnt]
    (let [cnt (if (or (<= cnt 0) (> cnt 100)) 3 cnt)]
      (map #(update % :created-at time.coerce/from-date)
           (db/query {:select/clause "note, voter_id, created_at"
-                     :where/map {:user-id user-id}
+                     :where/map {:user-id user-id
+                                 :chat-source-adapter (pr-str uuid)
+                                 :chat-source-room room}
                      :where/clause "note IS NOT NULL AND points > 0"
                      :order/clause "created_at DESC"
                      :limit/clause cnt})))))
 
 (defn get-high-scores
-  ([] (get-high-scores 10))
-  ([cnt]
+  ([] (get-high-scores nil 10))
+  ([chat-source] (get-high-scores chat-source 10))
+  ([{uuid :uuid room :room :as chat-source} cnt]
    (let [cnt (if (or (<= cnt 0) (> cnt 100)) 10 cnt)]
-     (db/query {:select/clause "user_id, SUM(points) as score"
-                :group/clause "user_id"
-                :having/clause "SUM(points) > 0"
-                :order/clause "score DESC"
-                :limit/clause cnt}))))
+     (db/query (merge {:select/clause "user_id, SUM(points) as score"
+                       :group/clause "user_id"
+                       :having/clause "SUM(points) > 0"
+                       :order/clause "score DESC"
+                       :limit/clause cnt}
+                      (when chat-source
+                        {:where/map {:chat-source-adapter (pr-str uuid)
+                                     :chat-source-room room}}))))))
 
+;; TODO add chat source
 (defn get-high-givers
   ([] (get-high-givers 10))
   ([cnt]
@@ -49,8 +61,8 @@
                 :limit/clause cnt}))))
 
 (defn delete-user!
+  "Delete user across all chat sources"
   [user-id]
   (doseq [id (map :id (db/query {:select/clause "id"
                                  :where/map {:user-id user-id}}))]
     (db/delete id)))
-
--- src/yetibot/core/webapp/handler.clj
+++ src/yetibot/core/webapp/handler.clj
@@ -58,9 +58,12 @@
             (run-server (app) {:join? false :daemon? true :port port}))))
 
 (defn stop-web-server []
-  (when-not (nil? @web-server)
-    (@web-server :timeout 100)
-    (reset! web-server nil)))
+  (if-not (nil? @web-server)
+    (do
+      (timbre/info "Stopping web server")
+      (reset! web-server nil)
+      (@web-server :timeout 100))
+    (timbre/info "Unable to stop web server")))
 
 (defn restart-web-server []
   (stop-web-server)
