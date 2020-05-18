b70af1381d6748136b5905af46a21dc15755c165
Trevor Hartman <trevorhartman@gmail.com>
2020-05-18 16:29:53 -0600

    Add blacklist/whitelist checking with test

--- src/yetibot/core/util/command.clj
+++ src/yetibot/core/util/command.clj
@@ -5,10 +5,50 @@
     [yetibot.core.models.help :as help]
     [yetibot.core.parser :refer [parser]]))
 
+(s/def ::whitelist-config (s/coll-of string?))
+(s/def ::blacklist-config (s/coll-of string?))
 
-(defn command-whitelist 
-  []
-  "TODO")
+(defn pattern-config-set
+  [spec path]
+  (set
+   (->>
+    (get-config spec path)
+    :value
+    (map re-pattern))))
+
+(defn whitelist []
+  (pattern-config-set ::whitelist-config [:command :whitelist]))
+
+(defn blacklist []
+  (pattern-config-set ::blacklist-config [:command :blacklist]))
+
+(defn any-match? [patterns s]
+  (some #(re-find % s) patterns))
+
+(defn is-command-enabled?
+  "Given a command prefix, determine whether or not it is enabled.
+
+   Users can specify either a whitelist collection of command patterns or a
+   blacklist collection of patterns, but not both.
+
+   If a whitelist is specified, all commands are disabled *except* those in the
+   whitelist.
+
+   If a blacklist is specified, all commands are enabled *except* those in the
+   blacklist."
+  [command]
+  (boolean
+   (cond
+     (and (seq (whitelist)) (seq (blacklist)))
+     (throw
+      (ex-info
+       "Invalid configuration: whitelist and blacklist cannot both be specified"
+       {:whitelist whitelist
+        :blacklist blacklist}))
+     (seq (whitelist)) (any-match? (whitelist) command)
+     (seq (blacklist)) (not (any-match? (blacklist) command))
+     ;; neither blacklist nor hitelist are configured
+     :else true)))
 
 (defn error?
   "Determine whether a value is an error map"
--- test/yetibot/core/test/util/command.clj
+++ test/yetibot/core/test/util/command.clj
@@ -1,25 +1,35 @@
 (ns yetibot.core.test.util.command
   (:require
-    ;; yetibot.core.commands.echo
-    [clojure.test :refer :all]
-    [yetibot.core.util.command :refer :all]))
+   yetibot.core.commands.echo
+   [midje.sweet :refer [fact facts => provided against-background]]
+   [yetibot.core.util.command :refer [whitelist blacklist
+                                      embedded-cmds is-command-enabled?]]))
 
-;; embedded commands
+(defn hi
+  []
+  "yes")
 
-(deftest test-embedded-cmds
-  (testing
-    "Embedded commands that aren't actually known commands are not parsed"
-    (is
-      (empty?
-        (embedded-cmds "`these` are the `invalid embedded commands`"))))
+(facts "About embedded commands"
+  (fact "Embedded commands that aren't actually known commands are not parsed"
+        (embedded-cmds "`these` are the `invalid embedded commands`") => empty?)
+  (fact
+   "Known embedded commands are properly extracted"
+   (embedded-cmds "`echo your temp:` wonder what the `temp 98101` is")
+   => [[:expr
+        [:cmd
+         [:words "echo" [:space " "] "your" [:space " "] "temp:"]]]]))
 
-  (testing
-    "Known embedded commands are properly extracted"
-    (is
-      (=
-       ;; temp shouldn't be included because it's not a command/alias in the
-       ;; test env
-       (embedded-cmds "`echo your temp:` wonder what the `temp 98101` is")
-       [[:expr
-         [:cmd
-          [:words "echo" [:space " "] "your" [:space " "] "temp:"]]]]))))
+(facts "About whitelists and blacklists"
+       (against-background
+        [(whitelist) => #{#"list"} (blacklist) => #{}]
+        (fact "echo is not whitelisted"
+              (is-command-enabled? "echo") => false)
+        (fact "echo is not whitelisted"
+              (is-command-enabled? "list") => true))
+
+       (against-background
+        [(whitelist) => #{} (blacklist) => #{#"list"}]
+        (fact "echo is not blacklisted"
+              (is-command-enabled? "echo") => true)
+        (fact "list is blacklisted"
+              (is-command-enabled? "list") => false)))
