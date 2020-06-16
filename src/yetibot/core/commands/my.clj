493524868aa075c3707b67582e1336ab26554b72
Trevor Hartman <trevorhartman@gmail.com>
2020-06-16 16:12:25 -0600

    Add our command in addition to my -a to list all known values

--- src/yetibot/core/commands/my.clj
+++ src/yetibot/core/commands/my.clj
@@ -1,11 +1,11 @@
 (ns yetibot.core.commands.my
   (:require [yetibot.core.hooks :refer [cmd-hook]]
             [taoensso.timbre :refer [info]]
-            [yetibot.core.db.my :as db]))
+            [yetibot.core.db.my :as db]
+            [yetibot.core.chat :as chat]))
 
-(defn format-kv
-  [{k :key v :value}]
-  (format "%s: %s" k v))
+(defn format-kv [{k :key v :value}] (format "%s: %s" k v))
+(defn format-v [{v :value}] (format "%s" v))
 
 (defn set-cmd
   "my <key> = <value> # set <key> = to <value> for the current user"
@@ -23,22 +23,35 @@
     (format "✓ Set %s = %s for %s" k v user-id)))
 
 (defn get-cmd
-  "my <key> # get the value of <key> for the current user"
-  [{k :match {user-id :username} :user}]
-  (let [[result] (db/query {:where/map {:key k}})]
+  "my <key> # get the value of <key> for the current user
+   my -a <key> # get the value of <key> for all users in the channel"
+  [{[_ all? k] :match {user-id :username} :user}]
+  (let [user-query (if all? {} {:user-id user-id})
+        result (db/query {:where/map (merge {:key k} user-query)})]
     (if result
-      {:result/value (format-kv result)
-       :result/data result}
+      (if all?
+        {:result/value (map format-v result)
+         :result/data result}
+        {:result/value (format-v (first result))
+         :result/data (first result)})
       {:result/error (format "No value found for key `%s` and user `%s`"
                              k user-id)})))
+
 (defn list-cmd
   "my # list all known keys for the current user"
   [{{user-id :username} :user}]
-  (let [results (db/query {:user-id user-id})]
+  (let [results (db/query {:where/map {:user-id user-id}})]
+    (info "list-cmd" (pr-str results))
     {:result/value (map format-kv results)
      :result/data results}))
 
 (comment
+  (db/create
+   {:user-id "john.doe"
+    :chat-source-adapter ":ybslack"
+    :chat-source-channel "local"
+    :key "zip"
+    :value "98104"})
   (db/create
    {:user-id "devth"
     :chat-source-adapter ":ybslack"
@@ -46,11 +59,24 @@
     :key "zip"
     :value "59101"})
 
-  (db/query {:where/map {:key "nope"}})
+  (db/query {:where/map {:key "zip"}})
 
   (db/find-all))
 
 (cmd-hook #"my"
           #"(\S+)\s+\=\s+(.+)" set-cmd
-          #".+" get-cmd
+          #"(-a)?\s*(.+)" get-cmd
           _ list-cmd)
+
+(defn our-cmd
+  "our <key> # get the value of <key> for all users in the channel (this is a shortcut for `my -a`)"
+  [{k :match}]
+  (let [result (db/query {:where/map (merge {:key k})})]
+    (if result
+      {:result/value (map format-v result)
+       :result/data result}
+      {:result/error (format "No value found for key `%s`" k)})))
+
+(cmd-hook #"our"
+          _ our-cmd)
+
