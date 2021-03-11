8a1ed7e213980fdf6318c88a604d7613f506aa87
gkspranger <greg@spranger.us>
2021-03-11 17:24:16 -0500

    load all commands in cmd fact

--- test/yetibot/core/test/commands/cmd.clj
+++ test/yetibot/core/test/commands/cmd.clj
@@ -3,8 +3,10 @@
    [midje.sweet :refer [fact => contains has-prefix just]]
    [matcher-combinators.midje :refer [match]]
    [yetibot.core.midje :refer [value data error]]
-   [yetibot.core.commands.cmd :refer [cmd]]))
+   [yetibot.core.commands.cmd :refer [cmd]]
+   [yetibot.core.loader :as loader]))
 
 (fact
  "cmd should work as expected"
+ (loader/load-commands)
  (cmd {:match "echo hi"}) => "hi")
