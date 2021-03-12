465de69bff8ef00a481444152921113dbff1de97
gkspranger <greg@spranger.us>
2021-03-11 19:08:44 -0500

    tests for core.loader.clj and small change v0.2 (#159)
    * save for now
    
    * codeclimate says dorun is non-idio -- so switching to doseq since we just want the side effects
    
    * be explicit about what "YB-all-namespaces" means
    
    * small optimization when finding and loading namespaces
    
    * found an issue where namespaces were being "double loaded" -- using distinct to make unique
    
    * put it all back to normal but include distinct fix
    
    * small movements
    
    * use original src loader.clj, drop test related to checking for dups
    
    * load all commands in cmd fact
    
    * try it all again

--- test/yetibot/core/test/commands/cmd.clj
+++ test/yetibot/core/test/commands/cmd.clj
@@ -1,10 +1,9 @@
 (ns yetibot.core.test.commands.cmd
   (:require
-   [midje.sweet :refer [fact => contains has-prefix just]]
-   [matcher-combinators.midje :refer [match]]
-   [yetibot.core.midje :refer [value data error]]
-   [yetibot.core.commands.cmd :refer [cmd]]))
+   [midje.sweet :refer [fact =>]]
+   [yetibot.core.commands.cmd :refer [cmd]]
+   [yetibot.core.loader :as loader]))
 
-(fact
- "cmd should work as expected"
- (cmd {:match "echo hi"}) => "hi")
+(fact "cmd should work as expected"
+      (loader/load-commands)
+      (cmd {:match "echo hi"}) => "hi")
