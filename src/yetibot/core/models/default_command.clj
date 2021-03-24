ba55226888046a6504cd1b202b91da171d578d08
gkspranger <greg@spranger.us>
2021-03-24 10:08:02 -0400

    put everything back to original state to verify all is OK

--- src/yetibot/core/models/default_command.clj
+++ src/yetibot/core/models/default_command.clj
@@ -1,23 +1,23 @@
 (ns yetibot.core.models.default-command
   "Determine which command to fallback to and whether fallback is enabled"
   (:require
-   [clojure.string :refer [blank?]]
-   [clojure.spec.alpha :as s]
-   [yetibot.core.config :refer [get-config]]))
+    [clojure.string :refer [blank?]]
+    [clojure.spec.alpha :as s]
+    [yetibot.core.config :refer [get-config]]))
 
 (s/def ::config any?)
 
-(defn configured-default-command
-  ([] (configured-default-command (get-config ::config [:default :command])))
-  ([cfg] (or (:value cfg)
-             "help")))
+(defn configured-default-command []
+  (or
+    (:value (get-config ::config [:default :command]))
+    "help"))
 
 (s/def ::text string?)
 
 (defn fallback-help-text-override
   "Optional config, may be nil"
-  ([] (fallback-help-text-override (get-config ::text [:command :fallback :help :text])))
-  ([cfg] (:value cfg)))
+  []
+  (:value (get-config ::text [:command :fallback :help :text])))
 
 (s/def ::fallback-commands-enabled-config string?)
 
@@ -26,10 +26,10 @@
    that doesn't exist. Default is true.
 
    In the future this may be channel specific but for now it is global."
-  ([] (fallback-enabled? (get-config ::fallback-commands-enabled-config
-                                     [:command :fallback :enabled])))
-  ([cfg] (let [{value :value} cfg]
-           (if-not (blank? value)
-             (not (= "false" value))
-             ;; enabled by default
-             true))))
+  []
+  (let [{value :value} (get-config ::fallback-commands-enabled-config
+                                   [:command :fallback :enabled])]
+    (if-not (blank? value)
+      (not (= "false" value))
+      ;; enabled by default
+      true)))
