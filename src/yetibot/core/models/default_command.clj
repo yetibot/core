005eaaaa1e99edbbb2f89a631717df33c9d3e6ad
gkspranger <greg@spranger.us>
2021-03-30 14:49:24 -0400

    mod models/defcmd to allow for arity/2 for testing custom config .. add tests for models/defcmd

--- src/yetibot/core/models/default_command.clj
+++ src/yetibot/core/models/default_command.clj
@@ -1,35 +1,47 @@
 (ns yetibot.core.models.default-command
   "Determine which command to fallback to and whether fallback is enabled"
-  (:require
-    [clojure.string :refer [blank?]]
-    [clojure.spec.alpha :as s]
-    [yetibot.core.config :refer [get-config]]))
+  (:require [clojure.spec.alpha :as s]
+            [yetibot.core.config :refer [get-config]]))
 
 (s/def ::config any?)
 
-(defn configured-default-command []
-  (or
-    (:value (get-config ::config [:default :command]))
-    "help"))
+(defn configured-default-command
+  "Gets the default command, as defined by the instance config.
+   Arity/2 allows for passing in custom config to compare against,
+   mainly used for testing."
+  ([] (configured-default-command (get-config ::config
+                                              [:default :command])))
+  ([cfg] (or (:value cfg) "help")))
+
+(comment
+  (configured-default-command {:value "findme"})
+  (configured-default-command {})
+  )
 
 (s/def ::text string?)
 
 (defn fallback-help-text-override
-  "Optional config, may be nil"
-  []
-  (:value (get-config ::text [:command :fallback :help :text])))
+  "Optional config for fallback help text. May be nil. Arity/2 allows for passing
+   in custom config to compare against, mainly used for testing."
+  ([] (fallback-help-text-override (get-config ::text
+                                               [:command :fallback :help :text])))
+  ([cfg] (:value cfg)))
 
 (s/def ::fallback-commands-enabled-config string?)
 
 (defn fallback-enabled?
   "Determine whether fallback commands are enabled when user enters a command
    that doesn't exist. Default is true.
+   In the future this may be channel specific but for now it is global.
+   Arity/2 allows for passing in custom config to compare against, mainly used
+   for testing."
+  ([] (fallback-enabled? (get-config ::fallback-commands-enabled-config
+                                     [:command :fallback :enabled])))
+  ([cfg] (->> (:value cfg) str (= "false") not)))
 
-   In the future this may be channel specific but for now it is global."
-  []
-  (let [{value :value} (get-config ::fallback-commands-enabled-config
-                                   [:command :fallback :enabled])]
-    (if-not (blank? value)
-      (not (= "false" value))
-      ;; enabled by default
-      true)))
+(comment
+  (fallback-enabled? {:value "false"})
+  (fallback-enabled? {:value false})
+  (fallback-enabled? {:value "true"})
+  (fallback-enabled? {:value "thiswilldefaulttotrue"})
+  )
---
+++ test/yetibot/core/test/models/default_command.clj
@@ -0,0 +1,37 @@
+(ns yetibot.core.test.models.default-command
+  (:require [yetibot.core.models.default-command :as dc]
+            [midje.sweet :refer [=> fact facts]]))
+
+(facts
+ "about configured-default-command"
+ (fact
+  "returns a custom default command"
+  (dc/configured-default-command {:value "findme"}) => "findme")
+ (fact
+  "returns the help command if no default command is set"
+  (dc/configured-default-command {:error :not-found}) => "help"))
+
+(facts
+ "about fallback-help-text-override"
+ (fact
+  "returns a custom help text message"
+  (dc/fallback-help-text-override {:value "findme"}) => "findme")
+ (fact
+  "returns nil when no custom text message is found"
+  (dc/fallback-help-text-override {:error :not-found}) => nil))
+
+(facts
+ "about fallback-enabled?"
+ (fact
+  "returns true when 'true' is passed"
+  (dc/fallback-enabled? {:value "true"}) => true)
+ (fact
+  "returns true when some non string-ified boolen is passed"
+  (dc/fallback-enabled? {:value "thiswillalsobetrue"}) => true)
+ (fact
+  "returns true when an error is returned"
+  (dc/fallback-enabled? {:error :not-found}) => true)
+ (fact
+  "returns false when 'false' is passed"
+  (dc/fallback-enabled? {:value "false"}) => false
+  (dc/fallback-enabled? {:value false}) => false))
