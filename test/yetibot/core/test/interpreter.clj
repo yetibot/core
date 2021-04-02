c43c8288503fa565960a670139c9f7a4221ce23c
gkspranger <greg@spranger.us>
2021-04-02 15:26:52 -0400

    test bad cmd call for echo (echop) and return `help echo` output (echo doc-string)

--- test/yetibot/core/test/interpreter.clj
+++ test/yetibot/core/test/interpreter.clj
@@ -4,6 +4,7 @@
    [yetibot.core.models.default-command :refer [fallback-enabled?
                                                 configured-default-command]]
    [yetibot.core.commands.help]
+   [yetibot.core.commands.echo]
    [yetibot.core.parser]
    [midje.sweet :refer [=> provided contains against-background
                         fact facts every-checker anything]]))
@@ -33,7 +34,16 @@
     cmd-result => (contains
                    (:doc (meta #'yetibot.core.commands.help/help-all-cmd)))
     cmd-result => (contains
-                   (:doc (meta #'yetibot.core.commands.help/help-for-topic))))))
+                   (:doc (meta #'yetibot.core.commands.help/help-for-topic)))))
+ (fact
+  "legit echo command is issued incorrectly (echop); handle-cmd sees there is a legit
+   similar command and throws it into the help command and gets back echo doc-string"
+  (require 'yetibot.core.commands.help :reload)
+  (against-background (configured-default-command) => "help")
+  (let [cmd-result (i/handle-cmd "echop" {})]
+    cmd-result => coll?
+    cmd-result => (contains
+                   (:doc (meta #'yetibot.core.commands.echo/echo-cmd))))))
 
 (facts
  "about handle-expr"
