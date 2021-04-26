7074cdf952249f7afb0a8e12a68a8ff8cc8b4720
gkspranger <greg@spranger.us>
2021-04-26 16:05:08 -0400

    new test .. this is a lot like using verify with mockito

--- test/yetibot/core/test/models/channel.clj
+++ test/yetibot/core/test/models/channel.clj
@@ -32,7 +32,29 @@
 (facts
  "about settings-for-chat-source"
  (fact
-  "does something"
+  "simply parses the map param and extracts correct keys to call channel-settings,
+   we are verifyig the function is called with the correct extracted params"
   (let [cmap {:uuid 123 :room "#abc123"}]
     (chan/settings-for-chat-source cmap) => true
     (provided (chan/channel-settings (:uuid cmap) (:room cmap)) => true))))
+
+(facts
+ "about find-key"
+ (fact
+  "gets the first result from a db query using the defined params, we are verifying
+   the db/query function is called with the correct params"
+  (let [uuid :someuuid
+        channel "#somechannel"
+        adapt-key "somekey"
+        key-result 123
+        query-wo-channel {:where/map {:chat-source-adapter (pr-str uuid)
+                                      :key adapt-key}}
+        query-w-channel (assoc-in query-wo-channel
+                                  [:where/map :chat-source-channel]
+                                  channel)]
+    ;; when no channel is present
+    (chan/find-key uuid nil adapt-key) => key-result
+    (provided (db/query query-wo-channel) => [key-result])
+    ;; when channel is real
+    (chan/find-key uuid channel adapt-key) => key-result
+    (provided (db/query query-w-channel) => [key-result]))))
