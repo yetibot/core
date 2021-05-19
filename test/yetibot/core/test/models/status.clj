7f62018b34d76ee07db487cdbc63f7b1e784b6db
gkspranger <greg@spranger.us>
2021-05-19 16:55:45 -0400

    the stub

--- test/yetibot/core/test/models/status.clj
+++ test/yetibot/core/test/models/status.clj
@@ -1,23 +1,9 @@
 (ns yetibot.core.test.models.status
-  (:require
-    [clojure.test :refer :all]
-    [yetibot.core.models.status :refer :all]
-    [clj-time
-     [format :refer [formatter unparse]]
-     [core :refer [day year month
-                   to-time-zone after?
-                   default-time-zone now time-zone-for-id date-time utc
-                   ago days weeks years months]]]))
+  (:require [yetibot.core.models.status :as sts]
+            [midje.sweet :refer [=> fact facts]]))
 
-;; helpers
-
-(defn- n-days-ago [n] (-> n days ago))
-
-(defn- status-map-for-n-days-ago [n]
-  {:timestamp (n-days-ago n)
-   :status (str "days ago: " n)})
-
-(defn- flat-status-map-n-days-ago [n]
-  ((juxt :timestamp :status) (status-map-for-n-days-ago n)))
-
-(defn- u [id] {:name (str id) :id id}) ;; user stub
+(facts
+ "about models.status"
+ (fact
+  "it always passes"
+  true => true))
