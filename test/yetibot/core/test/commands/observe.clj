e03d0dc9f5bcd831a55ef2d1f069034d6145eeaf
gkspranger <greg@spranger.us>
2021-05-20 12:13:51 -0400

    migrate tests to midje

--- test/yetibot/core/test/commands/observe.clj
+++ test/yetibot/core/test/commands/observe.clj
@@ -1,10 +1,23 @@
 (ns yetibot.core.test.commands.observe
-  (:require
-    [clojure.test :refer :all]
-    [clojure.string :refer [split]]
-    [yetibot.core.commands.observe :refer :all]))
+  (:require [midje.sweet :refer [=> facts fact contains]]
+            [yetibot.core.commands.observe :as obs]))
 
-(deftest observe-parse-test
-  (is (parse-observe-opts "-u lol x"))
-  (is (contains?  (parse-observe-opts "-u lol -ewat x") :errors )
-      "Should have errors with invalid event type"))
+(facts
+ "about commands.observe"
+ (fact
+  "it returns observed options that have no errors, contains the specified arg,
+   and recognizes the -u option with provided value"
+  (let [{:keys [arguments errors options]} (obs/parse-observe-opts
+                                            "-u lol x")]
+    errors => empty?
+    arguments => (contains "x")
+    (:user-pattern options) => "lol"))
+ (fact
+  "it will return an error when providing invalid options, but still will parse
+   whatever valid options it can find"
+  (let [{:keys [arguments errors options]} (obs/parse-observe-opts
+                                            "-u lol -ewat x")]
+    errors => coll?
+    (first errors) => (contains "Failed to validate")
+    arguments => (contains "x")
+    (:user-pattern options) => "lol")))
