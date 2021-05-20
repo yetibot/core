9882c2ac9c64975233116e9f145661bfe8c52de2
gkspranger <greg@spranger.us>
2021-05-20 10:33:34 -0400

    add test for format-sts

--- test/yetibot/core/test/models/status.clj
+++ test/yetibot/core/test/models/status.clj
@@ -1,9 +1,22 @@
 (ns yetibot.core.test.models.status
   (:require [yetibot.core.models.status :as sts]
-            [midje.sweet :refer [=> fact facts]]))
+            [yetibot.core.models.users :refer [get-user]]
+            [clj-time
+             [core :refer [ago hours]]
+             [coerce :refer [to-sql-time]]]
+            [midje.sweet :refer [=> fact facts provided anything
+                                 every-checker contains]]))
 
 (facts
  "about models.status"
  (fact
-  "it always passes"
-  true => true))
+  "it creates a coll of strings that contain '<username> <timestamp> <statys>' status info"
+  (let [name "zero"
+        status "hello world"
+        sts {:user-id "0"
+             :status status
+             :created-at (to-sql-time (-> 8 hours ago))}]
+    (first (sts/format-sts [sts])) => (every-checker
+                                       (contains status)
+                                       (contains name))
+    (provided (get-user anything (:user-id sts)) => {:name name}))))
