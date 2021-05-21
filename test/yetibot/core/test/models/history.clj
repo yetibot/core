7950d29c4a04bd0512ee2e1000da009cd18f5a29
gkspranger <greg@spranger.us>
2021-05-21 14:33:14 -0400

    migrate to midje, kondo clean src, allow for running kibit locally, update test README

--- test/yetibot/core/test/models/history.clj
+++ test/yetibot/core/test/models/history.clj
@@ -1,7 +1,6 @@
 (ns yetibot.core.test.models.history
   (:require [yetibot.core.models.history :as hst]
             [yetibot.core.db :as db]
-            [clojure.test :refer :all]
             [midje.sweet :refer [=> fact facts]]))
 
 (def chat-source {:adapter :slack :uuid :test :room "foo"})
@@ -12,48 +11,37 @@
 ;; we need a database
 (db/start)
 
-;; Another quick option would be to use a fixture to populate then rollback
-;; after the tests, something like:
-(defn populate
-  [f]
-  ;; this would require not using with-db-connection everywhere and having a
-  ;; reference to the db instead
-  ;; (sql/with-db-transaction [db db]
-  ;;   (sql/db-set-rollback-only! db)
-  ;;   (binding [db db]
-  ;;     ;; add 10 normal history items
-  ;;  (run! #(add-history (str "body" %)) (range 10))
-  ;;
-  ;;     ;; add 3 command history items
-  ;;  (run! add-history ["!echo" "!status" "!poke"])
-  (f))
-
-(use-fixtures :once populate)
-
 (def extra-query {:where/map {:chat-source-adapter (-> chat-source :uuid pr-str)
                               :is-yetibot false}})
 
-(deftest test-count-entities
-  (hst/count-entities extra-query))
-
-(deftest test-head
+(facts
+ "about core.models.history"
+ (fact
+  "it can count the number of entities without error"
+  (hst/count-entities extra-query) => int?)
+ (fact
+  "it can get the first lines of history without error"
   (hst/head 2 extra-query))
-
-(deftest test-tail
-  (count (hst/tail 2 extra-query)))
-
-(deftest test-random
-  (map? (hst/random extra-query)))
-
-(deftest test-grep
+ (fact
+  "it can get the last lines of history without error"
+  (hst/tail 2 extra-query))
+ (fact
+  "it can get history and show in random order without error"
+  (hst/random extra-query))
+ (fact
+  "it can search history for a specific pattern without error"
   (hst/grep "b.d+" extra-query))
-
-(deftest test-cmd-only-items
+ (fact
+  "it can get the history of commands only without error"
   (hst/cmd-only-items chat-source))
-
-(deftest test-non-cmd-items
+ (fact
+  "it can get the history of non-commands without error"
   (hst/non-cmd-items chat-source))
-
-(deftest last-chat-for-channel-test
-  (hst/last-chat-for-channel chat-source true)
-  (hst/last-chat-for-channel chat-source false))
+ (fact
+  "it can get the history of commands for a specific chat source without
+    error"
+  (hst/last-chat-for-channel chat-source true))
+ (fact
+  "it can get the history of 'normal chat' for a specific chat source
+    without error"
+  (hst/last-chat-for-channel chat-source false)))
