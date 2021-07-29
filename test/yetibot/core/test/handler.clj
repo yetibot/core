636cc84f41795395f2af6adee488732e204499bc
gkspranger <greg@spranger.us>
2021-07-29 09:21:40 -0400

    existing test are now working

--- test/yetibot/core/test/handler.clj
+++ test/yetibot/core/test/handler.clj
@@ -65,44 +65,49 @@
  "about add-user-message-to-history"
  (let [body "!echo hello"
        user {:id 123 :username "greg" :yetibot? false}
-       correlation-id (h/->correlation-id body user)
+       cmd-param-map (h/->cmd-param-map body
+                                        user
+                                        nil
+                                        nil)
+       yb-cmd-param-map (h/->cmd-param-map body
+                                           (assoc user
+                                                  :yetibot?
+                                                  true)
+                                           nil
+                                           nil)
        cs {:adapter :slack :uuid :test :room "#C123"}]
    (binding [i/*chat-source* cs]
      (fact
       "it will add to the history table a user message, with related message
        metadata"
-      (first (h/add-user-message-to-history body user correlation-id))
+      (first (h/add-user-message-to-history cmd-param-map))
       => (contains {:body body
                     :chat_source_room (:room cs)
-                    :correlation_id correlation-id
+                    :correlation_id (:correlation-id cmd-param-map)
                     :is_command true
                     :is_yetibot false
                     :user_id (str (:id user))
                     :user_name (:username user)}))
-
+     
      (fact
       "it will NOT add the user message to history because user is yetibot,
        and return nil"
-      (h/add-user-message-to-history nil {:yetibot? true} nil)
+      (h/add-user-message-to-history yb-cmd-param-map)
       => nil))))
 
 (facts
  "about handle-parsed-expr"
- (let [cs {:adapter :slack
-           :uuid :test
-           :room "#C123"}
-       user {:id 123 :username "greg" :yetibot? false}
-       yb-user {:id 456 :username "yetibot" :yetibot? true}]
+ (let [user {:id 123 :username "greg" :yetibot? false}
+       yb-user {:id 456 :username "yetibot" :yetibot? true}
+       cmd-param-map {:user user :yetibot-user yb-user}]
    (fact
     "it will return nil when no legit parse tree is passed in as an arg"
-    (h/handle-parsed-expr cs user yb-user nil) => nil)
+    (h/handle-parsed-expr cmd-param-map nil) => nil)
 
    (fact
     "it will return a map with the value of the response, related data,
      and settings"
-    (h/handle-parsed-expr cs
-                          user
-                          yb-user
+    (h/handle-parsed-expr cmd-param-map
                           (first
                            (:parsed-cmds (h/->parsed-message-info
                                           "!echo hello"))))
@@ -112,17 +117,13 @@
 
 (facts
  "about ->handled-expr-info"
- (let [cs {:adapter :slack
-           :uuid :test
-           :room "#C123"}
-       user {:id 123 :username "greg" :yetibot? false}
+ (let [user {:id 123 :username "greg" :yetibot? false}
        yb-user {:id 456 :username "yetibot" :yetibot? true}
+       cmd-param-map {:user user :yetibot-user yb-user}
        pe (first
            (:parsed-cmds (h/->parsed-message-info
                           "!echo hello")))
-       hpe (h/handle-parsed-expr cs
-                                 user
-                                 yb-user
+       hpe (h/handle-parsed-expr cmd-param-map
                                  pe)]
    (fact
     "it transforms a successfully handled parsed expression (command) and
@@ -139,26 +140,20 @@
  (let [body "!echo hello"
        user {:id 123 :username "greg" :yetibot? false}
        yb-user {:id 456 :username "yetibot" :yetibot? true}
-       correlation-id (h/->correlation-id body user)
+       cmd-param-map (h/->cmd-param-map body user yb-user true)
        cs {:adapter :slack :uuid :test :room "#C123"}
        pe (first
-           (:parsed-cmds (h/->parsed-message-info
-                          "!echo hello")))
-       hpe (h/handle-parsed-expr cs
-                                 user
-                                 yb-user
-                                 pe)]
+           (:parsed-cmds cmd-param-map))
+       hpe (h/handle-parsed-expr cmd-param-map pe)]
    (binding [i/*chat-source* cs]
      (fact
       "it will take the parsed YB bot response from a command and add
        it to the history database with the defined correlation id"
       (first (h/add-bot-response-to-history (h/->handled-expr-info hpe pe)
-                                            yb-user
-                                            true
-                                            correlation-id))
+                                            cmd-param-map))
       => (contains {:body "hello"
                     :chat_source_room (:room cs)
-                    :correlation_id correlation-id
+                    :correlation_id (:correlation-id cmd-param-map)
                     :is_command false
                     :is_yetibot true
                     :is_error false
@@ -169,9 +164,9 @@
       "it will NOT add the bot response to history because the
        `record-yetibot-response?` option is not true"
       (first (h/add-bot-response-to-history (h/->handled-expr-info hpe pe)
-                                            yb-user
-                                            false
-                                            correlation-id))
+                                            (assoc cmd-param-map
+                                                   :record-yetibot-response?
+                                                   false)))
       => nil))))
 
 (facts
