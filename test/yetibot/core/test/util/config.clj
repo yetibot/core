94d8b9fb91f4f105c76cac1cdad69f337329cde5
gkspranger <greg@spranger.us>
2022-01-26 12:51:44 -0500

    add tests for core.util.config

--- test/yetibot/core/test/util/config.clj
+++ test/yetibot/core/test/util/config.clj
@@ -1,8 +1,10 @@
 (ns yetibot.core.test.util.config
   (:require
-   [midje.sweet :refer [=> fact facts every-checker]]
+   [midje.sweet :refer [=> fact facts every-checker with-state-changes
+                        after contains]]
    [yetibot.core.util.config :as c]
-   [yetibot.core.loader :as ldr]))
+   [yetibot.core.loader :as ldr]
+   [clojure.java.io :as io]))
 
 (facts "about load-edn!"
        (fact "loading non-existent config file returns nil"
@@ -19,6 +21,11 @@
          (fact "get's valid map path and tests against spec"
                (c/get-config cfg ::ldr/url [:yetibot :url])
                => {:value "http://localhost:3003"})
+         (fact "get's valid map path (not in a collection) and tests against
+                spec"
+               (c/get-config {:url "http://localhost:3003"}
+                             ::ldr/url :url)
+               => {:value "http://localhost:3003"})
          (fact "get's invalid map path and tests against valid spec"
                (:error (c/get-config cfg
                                      ::ldr/url [:yetibot :doesnotexist]))
@@ -27,3 +34,33 @@
                (:error (c/get-config cfg
                                      ::ldr/plugin-config [:yetibot :url]))
                => :invalid)))
+
+(facts
+ "about config-exists?"
+ (fact
+  "it will return false when the supplied path does not exist on the file system
+   and will return true when the supplied path does exist on the file system"
+  (c/config-exists? "node.edn") => false
+  (c/config-exists? "config/config.sample.edn") => true))
+
+(facts
+ "about load-or-create-edn!"
+ (with-state-changes [(after :facts (io/delete-file "config/node-test.edn"))]
+   (fact
+    "it will create the config file if it doesn't exist and write to it the
+     default config values"
+    (c/load-or-create-edn! "config/node-test.edn") => c/default-config
+    (c/config-exists? "config/node-test.edn") => true))
+
+ (fact
+  "it will catch an exception when trying to load/create the config, log it,
+   and return nil"
+  (c/load-or-create-edn! (Exception. "i will fail")) => nil)
+
+ (fact
+  "it will load the config file if it does exist and return the hash map >>
+   small spot check on values in the config.sample.edn file"
+  (:yetibot (c/load-or-create-edn! "config/config.sample.edn"))
+  => (contains {:alphavantage {:key ""}}
+               {:default {:command "giphy"}}
+               {:nrepl {:port ""}})))
