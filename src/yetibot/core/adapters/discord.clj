883508e0b3cd6d0f5916236b4c44acb94b7b098b
Benson Trinh <trinh.benson@gmail.com>
2023-11-11 20:06:41 -0700

    wip

--- src/yetibot/core/adapters/discord.clj
+++ src/yetibot/core/adapters/discord.clj
@@ -1,13 +1,14 @@
 (ns yetibot.core.adapters.discord
   (:require [clojure.spec.alpha :as s]
             [clojure.core.async :refer [chan close!]]
-            [discljord.messaging :refer [start-connection! stop-connection! get-current-user! create-message!]]
+            [discljord.messaging :refer [get-guild-channels! start-connection! stop-connection! get-current-user! create-message!]]
             [discljord.connections :as discord-ws]
             [yetibot.core.models.users :as users]
             [discljord.events :refer [message-pump!]]
             [taoensso.timbre :refer [info]]
             [yetibot.core.adapters.adapter :as a]
-            [yetibot.core.chat :refer [chat-source]]))
+            [yetibot.core.handler :refer [handle-raw]]
+            [yetibot.core.chat :refer [base-chat-source chat-source *target* *adapter*]]))
 
 (s/def ::type #{"discord"})
 (s/def ::token string?)
@@ -20,41 +21,66 @@
     (users/get-user cs yetibot-uid)))
 
 (defmulti handle-event
-  (fn [event-type event-data]
+  (fn [event-type event-data _conn yetibot-user]
     event-type))
 
 (defmethod handle-event :message-create
-  [event-type event-data]
+  [event-type event-data _conn yetibot-user]
   (info "🎉 NEW EVENT! 🎉")
   (info "Event type:" event-type)
-  (info "Event data:" (pr-str event-data)))
-  ;; (if (= (:content event-data) "!disconnect")
-  ;;   (discord-ws/disconnect-bot! (:connection conn))
-  ;;   (when-not (:bot (:author event-data))
-  ;;     (create-message! (:rest conn) (:channel-id event-data) :content "Helloooo"))))
+  (info "Event data:" (pr-str event-data))
+  (info "Author: " (pr-str (:author event-data)))
+  (info "Channel ID: " (pr-str (:channel-id event-data)))
+  (if (= (:content event-data) "!disconnect")
+    (discord-ws/disconnect-bot! (:connection _conn))
+    (do
+      (info "Handling Message")
+      (let [user-model (users/create-user
+                        (event-data :author :username)
+                        (event-data :author :id))
+            message (:content event-data)]
+        (info "chat source: " (pr-str (chat-source (:channel-id event-data))))
+        (info "running handle-raw")
+        (binding [*target* (:channel-id event-data)]
+          (handle-raw
+           (chat-source (:channel-id event-data))
+           user-model
+           :message
+           @yetibot-user
+           {:body message}))))))
+
+
 
 (defn start
   "start the discord connection"
-  [adapter _conn config _connected? bot-id]
+  [adapter _conn config _connected? bot-id yetibot-user]
   (info "starting discord connection")
-  (let [event-channel (chan 100)
-        message-channel (discord-ws/connect-bot! (:token config) event-channel :intents #{:guilds :guild-messages})
-        rest-connection (start-connection! (:token config))]
-    (let [retcon {:event  event-channel
-                  :message message-channel
-                  :rest    rest-connection}]
-      (reset! _conn retcon)
 
-      (info (pr-str _conn))
-      (reset! bot-id {:id @(get-current-user! rest-connection)})
+  (binding [*adapter* adapter]
+    (let [event-channel (chan 100)
+          message-channel (discord-ws/connect-bot! (:token config) event-channel :intents #{:guilds :guild-messages})
+          rest-connection (start-connection! (:token config))]
+      (let [retcon {:event  event-channel
+                    :message message-channel
+                    :rest    rest-connection}]
+        (reset! _conn retcon)
 
-      ;; how do I pass in the connection into here?
-      (message-pump! event-channel handle-event))))
+        (info (pr-str _conn))
+        (reset! bot-id {:id @(get-current-user! rest-connection)})
+        (reset! yetibot-user @(get-current-user! rest-connection))
+        (message-pump! event-channel (fn [event-type event-data] (handle-event event-type event-data _conn yetibot-user)))))))
 
 
-(defn- channels [a])
 
-(defn- send-msg [a msg])
+(defn- channels [a]
+  (let [guild-channels (get-guild-channels!)]
+    (info "Guild Channels: " (pr-str guild-channels))
+    (guild-channels)))
+
+(defn- send-msg [adapter msg]
+  (info "Trying to send message: " msg)
+  (info "Adapter: " (pr-str adapter))
+  (create-message! (adapter :conn :rest) *target* :content msg))
 
 (defn stop
   "stop the discord connection"
@@ -109,7 +135,7 @@
     @connection-latency)
 
   (a/start [adapter]
-    (start adapter conn config connected? bot-id)))
+    (start adapter conn config connected? bot-id yetibot-user)))
 
 (defn make-discord
   [config]
