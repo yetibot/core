ebb4b75a4a5b919b3ef79a934decd75ca8152068
Benson Trinh <trinh.benson@gmail.com>
2024-01-02 12:06:53 -0700

    import naming conventions and getting reaction message content to be passed in properly

--- src/yetibot/core/adapters/discord.clj
+++ src/yetibot/core/adapters/discord.clj
@@ -1,18 +1,18 @@
 (ns yetibot.core.adapters.discord
-  (:require [clojure.spec.alpha :as s]
-            [clojure.core.async :as as]
-            [discljord.messaging :as m]
+  (:require [clojure.spec.alpha :as spec]
+            [clojure.core.async :as async]
+            [discljord.messaging :as messaging]
             [discljord.connections :as discord-ws]
             [yetibot.core.models.users :as users]
-            [discljord.events :as e]
-            [taoensso.timbre :as t]
-            [yetibot.core.adapters.adapter :as a]
-            [yetibot.core.handler :as h]
-            [yetibot.core.chat :as c]))
+            [discljord.events :as events]
+            [taoensso.timbre :as timbre]
+            [yetibot.core.adapters.adapter :as adapter]
+            [yetibot.core.handler :as handler]
+            [yetibot.core.chat :as chat]))
 
-(s/def ::type #{"discord"})
-(s/def ::token string?)
-(s/def ::config (s/keys :req-un [::type ::token]))
+(spec/def ::type #{"discord"})
+(spec/def ::token string?)
+(spec/def ::config (spec/keys :req-un [::type ::token]))
 
 (defmulti handle-event
   (fn [event-type event-data _conn yetibot-user]
@@ -21,29 +21,37 @@
 ;; also has :message-reaction-remove
 (defmethod handle-event :message-reaction-add
   [event-type event-data _conn yetibot-user]
-  (if (and
-       (= (-> event-data :emoji :name) "❌")
-       (= (:message-author-id event-data) (:id @yetibot-user)))
-    (let [message-id (:message-id event-data)]
-      (m/delete-message! (:rest @_conn) (:channel-id event-data) message-id))
-    (if (not= (:message-author-id event-data) (:id @yetibot-user))
-      (t/debug "You can only delete messages from Yetibot")
-      (let [user-model (users/create-user
-                        (-> event-data
-                            :message-author-id)
-                        (-> event-data
-                            :member
-                            :user
-                            :username))
-            cs (assoc (c/chat-source (:channel-id event-data))
-                      :raw-event event-data)
-            reaction (-> event-data :emoji :name)]
-        (h/handle-raw cs user-model :react yetibot-user
-                    {:reaction reaction
-                                   ;; body of the message reacted to
-                     :body "Not working yet"
-                                   ;; user of the message that was reacted to
-                     :message-user (:message-author-id event-data)})))))
+  (let [message-id (:message-id event-data)
+        channel-id (:channel-id event-data)
+        message-author-id (:message-author-id event-data)
+        username (-> event-data
+                     :member
+                     :user
+                     :username)
+        emoji-name (-> event-data
+                       :emoji
+                       :name)
+        rest-conn (:rest @_conn)
+        yetibot? (= message-author-id (:id @yetibot-user))]
+    (if (and
+         (= (-> event-data :emoji :name) "❌")
+         (= yetibot? true))
+      (messaging/delete-message! rest-conn channel-id message-id)
+      (if (= yetibot? true)
+        (timbre/debug "We don't handle " (emoji-name) "from yetibot")
+        (let [cs (assoc (chat/chat-source channel-id)
+                        :raw-event event-data)
+              user-model (assoc (users/get-user cs message-author-id)
+                                :yetibot? yetibot?)
+              message-content (:content @(messaging/get-channel-message! rest-conn channel-id message-id))]
+          (handler/handle-raw
+           cs
+           user-model
+           :react
+           @yetibot-user
+           {:reaction emoji-name
+            :body message-content
+            :message-user message-author-id}))))))
 
 
 (defmethod handle-event :message-create
@@ -57,19 +65,16 @@
                           :username)
                       (event-data :author :id))
           message (:content event-data)
-          cs (assoc (c/chat-source (:channel-id event-data))
+          cs (assoc (chat/chat-source (:channel-id event-data))
                     :raw-event event-data)]
-      (t/debug "chat source: " (pr-str cs))
-      (t/debug "running handle-raw")
-
-      (binding [c/*target* (:channel-id event-data)]
-        (h/handle-raw
-         (c/chat-source (:channel-id event-data))
+      (binding [chat/*target* (:channel-id event-data)]
+        (handler/handle-raw
+         cs
          user-model
          :message
          @yetibot-user
          {:body message})))
-    (t/debug "Message from Yetibot => ignoring")))
+    (timbre/debug "Message from Yetibot => ignoring")))
 
 (defn start
   "start the discord connection"
@@ -78,38 +83,38 @@
     connected? :connected?
     bot-id :bot-id
     yetibot-user :yetibot-user :as adapter}]
-  (t/debug "starting discord connection")
+  (timbre/debug "starting discord connection")
 
-  (binding [c/*adapter* adapter]
-    (let [event-channel (as/chan 100)
+  (binding [chat/*adapter* adapter]
+    (let [event-channel (async/chan 100)
           message-channel (discord-ws/connect-bot! (:token config) event-channel :intents #{:guilds :guild-messages :guild-message-reactions :direct-messages :direct-message-reactions})
-          rest-connection (m/start-connection! (:token config))
+          rest-connection (messaging/start-connection! (:token config))
           retcon {:event  event-channel
                   :message message-channel
                   :rest    rest-connection}]
       (reset! conn retcon)
 
-      (t/debug (pr-str conn))
+      (timbre/debug (pr-str conn))
 
       (reset! connected? true)
-      (reset! bot-id {:id @(m/get-current-user! rest-connection)})
-      (reset! yetibot-user @(m/get-current-user! rest-connection))
-      (e/message-pump! event-channel (fn [event-type event-data] (handle-event event-type event-data conn yetibot-user))))))
+      (reset! bot-id {:id @(messaging/get-current-user! rest-connection)})
+      (reset! yetibot-user @(messaging/get-current-user! rest-connection))
+      (events/message-pump! event-channel (fn [event-type event-data] (handle-event event-type event-data conn yetibot-user))))))
 
 (defn- channels [a]
-  (let [guild-channels (m/get-guild-channels!)]
-    (t/debug "Guild Channels: " (pr-str guild-channels))
+  (let [guild-channels (messaging/get-guild-channels!)]
+    (timbre/debug "Guild Channels: " (pr-str guild-channels))
     (guild-channels)))
 
 (defn- send-msg [{:keys [conn]} msg]
-  (m/create-message! (:rest @conn) c/*target* :content msg))
+  (messaging/create-message! (:rest @conn) chat/*target* :content msg))
 
 (defn stop
   "stop the discord connection"
   [{:keys [conn] :as adapter}]
-  (t/debug "Closing Discord" (a/uuid adapter))
-  (m/stop-connection! (:message @conn))
-  (as/close!           (:event @conn)))
+  (timbre/debug "Closing Discord" (adapter/uuid adapter))
+  (messaging/stop-connection! (:message @conn))
+  (async/close!           (:event @conn)))
 
 (defrecord Discord
            [config
@@ -121,7 +126,7 @@
             should-ping?
             yetibot-user]
 
-  a/Adapter
+  adapter/Adapter
 
   (a/uuid [_] (:name config))
 
@@ -143,7 +148,7 @@
      "Discord bots such as myself can't leave channels on their own. Use "
      "/kick from the channel you'd like me to leave instead. 👊"))
 
-  (a/chat-source [_ channel] (c/chat-source channel))
+  (a/chat-source [_ channel] (chat/chat-source channel))
 
   (a/stop [adapter] (stop adapter))
 
