bc89d2214d6413d8583366982445d54e9eb27ec3
Trevor Hartman <th@devth.com>
2026-03-18 12:16:37 -0600

    Add logging to discord emoji command

--- src/yetibot/core/commands/discord.clj
+++ src/yetibot/core/commands/discord.clj
@@ -13,10 +13,14 @@
     {:result/error "discord emoji only works on Discord 🎈"}
     (let [adapter (get @a/adapters (:uuid chat-source))
           channel-id (:room chat-source)
+          _ (info "discord emoji: channel-id" channel-id "chat-source" (pr-str chat-source))
           rest-conn (:rest @(:conn adapter))
           channel-info @(messaging/get-channel! rest-conn channel-id)
+          _ (info "discord emoji: channel-info" (pr-str channel-info))
           guild-id (:guild-id channel-info)
-          emojis @(messaging/list-guild-emojis! rest-conn guild-id)]
+          _ (info "discord emoji: guild-id" guild-id)
+          emojis @(messaging/list-guild-emojis! rest-conn guild-id)
+          _ (info "discord emoji: emojis count" (count emojis))]
       {:result/data emojis
        :result/value (map :name emojis)})))
 
