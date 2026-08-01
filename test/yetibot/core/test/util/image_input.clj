ffb27182b18d70382f98b752c6ba7810605b8adb
Yetibot <yetibot@yetibot.com>
2026-08-01 15:41:25 +0000

    Generate default avatar fallbacks and label images for Gemini alignment

--- test/yetibot/core/test/util/image_input.clj
+++ test/yetibot/core/test/util/image_input.clj
@@ -10,14 +10,16 @@
     (img-input/extract-images
       "<@123456789>"
       {:raw-event {:mentions [{:id "123456789" :avatar "avatar123" :username "alice"}]}})
-    => {:prompt "@alice" :image-urls ["https://cdn.discordapp.com/avatars/123456789/avatar123.png?size=256"]})
+    => {:prompt "(Image 1 is @alice) @alice"
+        :image-urls ["https://cdn.discordapp.com/avatars/123456789/avatar123.png?size=256"]})
 
   (fact
     "a hardcoded user mention extracts the custom mario death meme avatar"
     (img-input/extract-images
       "<@269292446041636866>"
       {:raw-event {:mentions [{:id "269292446041636866" :avatar "someavatar" :username "bob"}]}})
-    => {:prompt "@bob" :image-urls ["https://i.imgflip.com/4/9omh8s.jpg"]})
+    => {:prompt "(Image 1 is @bob) @bob"
+        :image-urls ["https://i.imgflip.com/4/9omh8s.jpg"]})
 
   (fact
     "user mentions are extracted and sorted in typing order, not snowflake order"
@@ -25,7 +27,7 @@
       "draw <@333333333> and <@111111111>"
       {:raw-event {:mentions [{:id "111111111" :avatar "av1" :username "alice"}
                              {:id "333333333" :avatar "av3" :username "charlie"}]}})
-    => {:prompt "draw @charlie and @alice"
+    => {:prompt "(Image 1 is @charlie. Image 2 is @alice) draw @charlie and @alice"
         :image-urls ["https://cdn.discordapp.com/avatars/333333333/av3.png?size=256"
                      "https://cdn.discordapp.com/avatars/111111111/av1.png?size=256"]})
 
@@ -34,7 +36,7 @@
     (img-input/extract-images
       "draw me as a wizard"
       {:raw-event {:author {:id "222222222" :avatar "av2" :username "bob"}}})
-    => {:prompt "draw @bob as a wizard"
+    => {:prompt "(Image 1 is @bob) draw @bob as a wizard"
         :image-urls ["https://cdn.discordapp.com/avatars/222222222/av2.png?size=256"]})
 
   (fact
@@ -42,7 +44,7 @@
     (img-input/extract-images
       "draw ME flying"
       {:raw-event {:author {:id "222222222" :avatar "av2" :username "bob"}}})
-    => {:prompt "draw @bob flying"
+    => {:prompt "(Image 1 is @bob) draw @bob flying"
         :image-urls ["https://cdn.discordapp.com/avatars/222222222/av2.png?size=256"]})
 
   (fact
@@ -59,6 +61,14 @@
       "draw me and <@111111111>"
       {:raw-event {:author {:id "222222222" :avatar "av2" :username "bob"}
                    :mentions [{:id "111111111" :avatar "av1" :username "alice"}]}})
-    => {:prompt "draw @bob and @alice"
+    => {:prompt "(Image 1 is @bob. Image 2 is @alice) draw @bob and @alice"
         :image-urls ["https://cdn.discordapp.com/avatars/222222222/av2.png?size=256"
-                     "https://cdn.discordapp.com/avatars/111111111/av1.png?size=256"]}))
+                     "https://cdn.discordapp.com/avatars/111111111/av1.png?size=256"]})
+
+  (fact
+    "a user without an avatar gets the default discord avatar URL"
+    (img-input/extract-images
+      "<@123456789>"
+      {:raw-event {:mentions [{:id "123456789" :username "alice"}]}})
+    => {:prompt "(Image 1 is @alice) @alice"
+        :image-urls ["https://cdn.discordapp.com/embed/avatars/5.png"]}))
